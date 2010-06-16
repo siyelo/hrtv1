d634156029e6c522e7131acc6fd1663708189cb6
Greg <greg@TheIvoryTower.(none)>
2010-06-16 03:34:47 -0400

    add active_scaffold + render_component

--- vendor/plugins/active_scaffold/test/mock_app/app/controllers/application_controller.rb
+++ vendor/plugins/active_scaffold/test/mock_app/app/controllers/application_controller.rb
@@ -7,7 +7,4 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
-  ActiveScaffold.set_defaults do |config| 
-    config.ignore_columns.add [:created_at, :updated_at, :lock_version]
-  end 
 end

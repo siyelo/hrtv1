340caa3543d5df311bf32e2f3610769eeaef11c2
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-06-20 16:51:10 +0200

    add seed data for model and field help

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,9 @@ model_helps = open ('db/seed_files/model_help.yaml') { |f| YAML.load(f) }
 def seed_model_help_from_yaml doc
   doc.each do |h|
     model_help_attribs = h.last
+    #TODO replace line below, may cause trouble during deployment
+    #     can replace after add rescue below
+    `touch db/seed_files/#{model_help_attribs["model_name"]}_help.yaml`
     seed_model_and_field_help model_help_attribs
   end
 end
@@ -19,16 +22,20 @@ end
 def seed_model_and_field_help  attribs 
   model_help=ModelHelp.find_or_create_by_model_name attribs["model_name"]
   model_help.update_attributes attribs
+  seed_field_help_from_yaml model_help
 end
 
-def seed_field_help_from_yaml model_help, field_help_attribs
-  field_help_attribs.each do |a|
-    fh = model_help.find_or_create_by_attribute_name field_help_attribs[:attribute_name]
-    fh.update_attributes a
+def seed_field_help_from_yaml model_help
+  field_helps = open ("db/seed_files/#{model_help.model_name}_help.yaml") { |f| YAML.load(f) }
+  if field_helps
+    field_help_attribs = field_helps.map { |h| h.last }
+    field_help_attribs.each do |a|
+      fh = model_help.field_help.find_or_create_by_attribute_name a["attribute_name"]
+      fh.update_attributes a
+    end #TODO add rescue if fields file is not there
   end
 end
 
 seed_model_help_from_yaml model_helps
-#[Project, Activity, LineItem].each { |k| create_help_for_model k }
 
-
+#TODO seed code values

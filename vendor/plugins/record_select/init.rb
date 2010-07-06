78d1dc21792e86797a528e86feefcd54e5b8aee8
Greg Nelson <railsdev@railsdev-laptop.(none)>
2010-07-06 10:11:57 +0200

    remove file system changes for heroku

--- vendor/plugins/record_select/init.rb
+++ vendor/plugins/record_select/init.rb
@@ -9,12 +9,13 @@ $LOAD_PATH.shift
 ActionController::Base.send(:include, RecordSelect)
 ActionView::Base.send(:include, ActionView::Helpers::RecordSelectHelpers)
 
-['stylesheets', 'images', 'javascripts'].each do |asset_type|
-  public_dir = File.join(RAILS_ROOT, 'public', asset_type, 'record_select')
-  local_dir = File.join(File.dirname(__FILE__), 'assets', asset_type)
-  FileUtils.mkdir public_dir unless File.exists? public_dir
-  Dir.entries(local_dir).each do |file|
-    next if file =~ /^\./
-    FileUtils.cp File.join(local_dir, file), public_dir
-  end
+# heroku read only file system
+#['stylesheets', 'images', 'javascripts'].each do |asset_type|
+#  public_dir = File.join(RAILS_ROOT, 'public', asset_type, 'record_select')
+#  local_dir = File.join(File.dirname(__FILE__), 'assets', asset_type)
+#  FileUtils.mkdir public_dir unless File.exists? public_dir
+#  Dir.entries(local_dir).each do |file|
+#    next if file =~ /^\./
+#    FileUtils.cp File.join(local_dir, file), public_dir
+#  end
 end

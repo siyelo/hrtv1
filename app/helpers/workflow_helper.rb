994fb02e291069b0fe34514850dd4da3fc4c5e32
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-07-07 15:28:22 +0200

    right way to do AS customization using list.html.erb

--- app/helpers/workflow_helper.rb
+++ app/helpers/workflow_helper.rb
@@ -1,7 +1,7 @@
 module WorkflowHelper
   #TODO write integration test that just walks through these
   # following links and testing for the right active scaffold name heading
-  @@map = { "start" => "projects/data_entry", "projects/data_entry" => "funding_flows/funding_sources", "funding_flows/funding_sources" => "funding_flows/providers",
+  @@map = { "start" => "projects/index", "projects/index" => "funding_flows/funding_sources", "funding_flows/funding_sources" => "funding_flows/providers",
             "funding_flows/providers" => "activities/data_entry" , "activities/data_entry" => "show"}
 
   def workflow_start

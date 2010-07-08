7cb9433c6f72280ce2c6b008fe847022a21fdf76
Glenn Roberts <glenn.roberts@siyelo.com>
2010-07-08 18:00:23 +0200

    fix projects hardcoded url in wflow helper - other helpers prob broken

--- app/helpers/workflow_helper.rb
+++ app/helpers/workflow_helper.rb
@@ -1,20 +1,9 @@
 module WorkflowHelper
   #TODO write integration test that just walks through these
   # following links and testing for the right active scaffold name heading
-<<<<<<< HEAD
 
-  #GR: using AASM will be more concise here. Also we cant use route helpers in this module
-  @@map = { "start" => "projects", "projects" => "funding_flows/funding_sources", "funding_flows/funding_sources" => "funding_flows/providers",
-=======
-  #TODO broken at the funding_flows steps now that removed routes
-<<<<<<< HEAD
-  @@map = { "start" => "projects/index", "projects/index" => "funding_flows/funding_sources", "funding_flows/index" => "funding_flows/providers",
->>>>>>> 76b2b7a... refactoring broke workflow links, funding_flows need attention
-            "funding_flows/providers" => "activities/index" , "activities/index" => "show"}
-=======
-  @@map = { "start" => "projects/index", "projects/index" => "funding_sources", "funding_sources/index" => "providers",
-            "providers/index" => "activities" , "activities/index" => "show"}
->>>>>>> 89eb0b4... nicer routes and workflow
+  @@map = { "start" => "projects", "projects" => "funding_flows/funding_sources", "funding_flows/index" => "funding_flows/providers",
+             "funding_flows/providers" => "activities/index" , "activities/index" => "show"}
 
   def workflow_start
     "/"+@@map["start"]
--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,22 @@
 ActionController::Routing::Routes.draw do |map|
-<<<<<<< HEAD
 
-  # GR - we'll namespace these as soon as the tests are running
-  #map.namespace :ngo do |ngo|
-  #  ngo.resources :project, :active_scaffold => true
-  #end
-=======
-  #ugly manual paths
   map.funding_sources_data_entry "funding_sources", :controller => 'funding_sources', :action => 'index'
   map.providers_data_entry "providers", :controller => 'providers', :action => 'index'
   %w[activities funding_flows projects providers funding_sources].each do |model|
     map.create_from_file model+"/create_from_file", :controller => model, :action => "create_from_file"
   end
 
->>>>>>> 76b2b7a... refactoring broke workflow links, funding_flows need attention
 
-<<<<<<< HEAD
-  map.resources :activity, :active_scaffold => true
-  map.resources :indicator, :active_scaffold => true
-  map.resources :line_item, :active_scaffold => true
-  map.resources :project, :active_scaffold => true
+  map.resources :activities, :active_scaffold => true
+  map.resources :indicators, :active_scaffold => true
+  map.resources :line_items, :active_scaffold => true
+  map.resources :projects, :active_scaffold => true
   map.resources :comments, :active_scaffold => true
   map.resources :field_helps, :active_scaffold => true
   map.resources :model_helps, :active_scaffold => true
-  map.resources :organization, :collection => {:browse => :get}, :active_scaffold => true
-=======
-  map.resources :activities, :active_scaffold => true
-  map.resources :indicators, :active_scaffold => true 
-  map.resources :line_items, :active_scaffold => true 
-  map.resources :projects, :active_scaffold => true 
-  map.resources :comments, :active_scaffold => true 
-  map.resources :field_helps, :active_scaffold => true 
-  map.resources :model_helps, :active_scaffold => true 
   map.resources :organizations, :collection => {:browse => :get}, :active_scaffold => true
->>>>>>> 89eb0b4... nicer routes and workflow
   map.resources :funding_flows, :active_scaffold => true
   map.resources :codes, :active_scaffold => true
-
-
 
   map.page_comments "page_comments/:id", :controller => 'comments', :action => 'index', :type => 'ModelHelp'
 
@@ -49,35 +28,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.root :controller => 'static_page', :action => 'index' #a replacement for public/index.html
 
-<<<<<<< HEAD
+  #TODO remove these
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 
-=======
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
- # map.connect ':controller/:action/:id'
- # map.connect ':controller/:action/:id.:format'
->>>>>>> 76b2b7a... refactoring broke workflow links, funding_flows need attention
 end

f8dc585cb257adc266e7ca4cf7b0dc785b658997
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-07-09 08:35:53 +0200

    improve comments view

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
-  @@shown_columns = [:title, :comment, :commentable]
+  @@shown_columns = [:title, :comment, :commentable, :created_at]
   @@create_columns = [:title, :comment]
   
   active_scaffold :comment do |config|
     config.columns =  @@shown_columns
-#    list.sorting = {:created_on => 'DESC'}
+    config.columns[:commentable].label = "Comment On"
+    list.sorting = {:created_at => 'DESC'}
   end
 
 end

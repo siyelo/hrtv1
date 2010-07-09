f65b782cf07574f833c88d29dde469378d142ed4
Glenn Roberts <glenn.roberts@siyelo.com>
2010-07-09 16:49:16 +0200

    remove ids from breakdown rows

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,5 @@
 config.gem "ruby-debug"
-config.gem "rails-footnotes"
+#config.gem "rails-footnotes"
 
 config.cache_classes                                 = false
 config.whiny_nils                                    = true

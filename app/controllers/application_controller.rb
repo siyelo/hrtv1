b2d7e029b00d9c1e7f5a572baeaeb091ee5fae4f
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-07-14 00:58:25 +0200

    remove show links in scaffolds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
 
   ActiveScaffold.set_defaults do |config| 
     config.ignore_columns.add [:created_at, :updated_at, :lock_version]
+    config.actions.exclude :show
     config.list.empty_field_text = "------"
     config.list.pagination = false
     config.create.persistent = true

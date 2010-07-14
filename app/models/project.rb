c0a77cfda58fa8063358c90e70dc6f775c699bc5
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-07-14 18:53:57 +0200

    show more fields on codes assignment

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,8 +18,10 @@ class Project < ActiveRecord::Base
   end
 
   def valid_providers
-    f=funding_flows.find(:all, :select => "organization_id_to", :conditions => ["organization_id_from = ?", 
-                       Organization.find_by_name("self").id])
+    f=funding_flows.find(:all, :select => "organization_id_to",
+      :conditions =>
+      ["organization_id_from = ?", Organization.find_by_name("self").id])
+
     r=f.collect {|f| f.organization_id_to}
     r
   end

5249ee2212ddd0eb51998056e49260608142350b
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-07-15 15:49:50 +0200

    fix file upload columns, usability changes

--- app/controllers/funding_flows_controller.rb
+++ app/controllers/funding_flows_controller.rb
@@ -1,6 +1,7 @@
 class FundingFlowsController < ApplicationController
   @@shown_columns = [:project, :from, :to, :raw_provider, :budget, :spend_q1]
-  @@create_columns = [:project, :from, :to, :raw_provider, :budget, :spend_q1, :spend_q2, :spend_q3, :spend_q4]
+  @@create_columns = [:project, :from, :to, :budget, :spend_q1, :spend_q2, :spend_q3, :spend_q4]
+  @@update_columns = [:project, :from, :to, :raw_provider, :budget, :spend_q1, :spend_q2, :spend_q3, :spend_q4]
   @@columns_for_file_upload = @@shown_columns.map {|c| c.to_s} # TODO extend feature, locations for instance won't work
 
   map_fields :create_from_file,
@@ -15,7 +16,7 @@ class FundingFlowsController < ApplicationController
     config.columns[:raw_provider].form_ui = :textarea
     config.columns[:raw_provider].options = {:cols => 50, :rows => 3}
     config.columns[:raw_provider].inplace_edit = true
-    config.columns[:raw_provider].label = "Organization Text"
+    config.columns[:raw_provider].label = "Organization Text from File Import"
 
     config.nested.add_link("Comments", [:comments])
     config.columns[:comments].association.reverse = :commentable
@@ -26,18 +27,17 @@ class FundingFlowsController < ApplicationController
     [:from, :to ].each do |c|
       config.columns[c].form_ui=:select
       config.columns[c].inplace_edit = true
-
-    config.create.columns = @@create_columns
-    config.update.columns = config.create.columns
     end
+    config.create.columns = @@create_columns
+    config.update.columns = @@update_columns
    # config.columns[:to].options = {:selected => 1260} #TODO add default provider self later
     config.columns[:budget].inplace_edit = true
-    config.columns[:budget].label = "Budget for RFY 10-11 (upcoming)"
+    config.columns[:budget].label = "Budget for GOR FY 10-11 (upcoming)"
     %w[q1 q2 q3 q4].each do |quarter|
       c="spend_"+quarter
       c=c.to_sym
       config.columns[c].inplace_edit = true
-      config.columns[c].label = "Expenditure in RFY 09-10 "+quarter.capitalize
+      config.columns[c].label = "Expenditure in GOR FY 09-10 "+quarter.capitalize
     end
   end
 

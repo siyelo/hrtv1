d344d6096ebecf2777488b89697267ce73ad1df1
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-07-19 13:05:31 +0200

    fix test::unit unit tests

--- test/unit/funding_flow_test.rb
+++ test/unit/funding_flow_test.rb
@@ -5,29 +5,8 @@ class FundingFlowTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
-  
-  test "comes from an org" do
-    f=FundingFlow.new
-    f.save
-    assert f.from == nil
-    o=f.create_from
-    assert Organization.count == 1
-    f.save
-    f=FundingFlow.find(f.id)
-    o=Organization.find(o.id)
-    assert f.from == o
-  end
-  test "goes to an org" do
-    f=FundingFlow.new
-    f.save
-    assert f.to == nil
-    o=f.create_to
-    assert Organization.count == 1
-    f.save
-    f=FundingFlow.find(f.id)
-    o=Organization.find(o.id)
-    assert f.to == o
-  end
+  should belong_to :to
+  should belong_to :from
   test "can add errors array to this object w method" do
     f=FundingFlow.new
     errors=["ho dang", "ouchies"]

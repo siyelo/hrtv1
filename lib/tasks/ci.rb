5d31b860d028d6ead4d1f0be7a7aeebf9cf2b07c
Glenn Roberts <glenn.roberts@siyelo.com>
2010-07-21 18:44:48 +0200

    get ci script running properly

--- lib/tasks/ci.rb
+++ lib/tasks/ci.rb
@@ -5,6 +5,7 @@
 # to configure your CI Joe server, add this
 #   $ git config --add cijoe.runner "ruby lib/tasks/ci.rb"
 #
+EXIT_OK = 0
 EXIT_NOK = 1
 
 # copy in any yamls not in the repo
@@ -12,11 +13,14 @@ system "cp config/database.yml.sample config/database.yml"
 system "cp config/settings.secret.example.yml config/settings.secret.yml"
 
 # run db creation and seeding
-setup_and_seeded = system "RAILS_ENV=cucumber rake setup --trace"
-exit EXIT_NOK unless setup_and_seeded
+setup_and_seeded = system "export RAILS_ENV=cucumber && rake setup --trace"
+unless setup_and_seeded
+  puts "environment setup failed"
+  exit EXIT_NOK
+end
 
 # cijoe build
-built = system "RAILS_ENV=cucumber rake -s test"
+built = system "export RAILS_ENV=cucumber && rake -s test"
 exit EXIT_NOK unless built
 
-return result
+exit EXIT_OK

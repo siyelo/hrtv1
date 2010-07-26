c6f2267d5c1b2f64e12db6d0ac48e9d4aab6313a
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-07-26 19:16:30 +0200

    remove unneeded migration

--- db/migrate/20100616182242_remove_old_activity_fields.rb
+++
@@ -1,9 +0,0 @@
-class RemoveOldActivityFields < ActiveRecord::Migration
-  def self.up
-    remove_column :activities, :newfield
-  end
-
-  def self.down
-    add_column :activities, :newfield
-  end
-end

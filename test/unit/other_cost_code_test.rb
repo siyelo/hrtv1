376244f37c74555b8aaf0a2a15c50bee371a1f52
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-07-27 20:24:54 +0200

    fix copied test to not be a copy

--- test/unit/other_cost_code_test.rb
+++ test/unit/other_cost_code_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CostCategoryTest < ActiveSupport::TestCase
+class OtherCostCodeTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true

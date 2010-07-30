94b3bac6914188ea3674714d187c53c427eca248
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-07-30 06:34:18 +0200

    seed other cost codes

--- db/seeds.rb
+++ db/seeds.rb
@@ -94,6 +94,23 @@ FasterCSV.foreach("db/seed_files/cost_categories.csv", :headers=>true) do |row|
   #puts "  #{c.id}"
 end
 
+puts "Loading other_cost_codes.csv..."
+OtherCostCode.delete_all
+FasterCSV.foreach("db/seed_files/other_cost_codes.csv", :headers=>true) do |row|
+
+  c             = OtherCostCode.new
+  c.external_id = row["id"]
+  p             = OtherCostCode.find_by_external_id(row["parent_id"])
+  c.parent_id   = p.id unless p.nil?
+  c.description = row["description"]
+
+  c.short_display=row["short_display"]
+
+  print "."
+  puts "error on #{row}" unless c.save!
+  #puts "  #{c.id}"
+end
+
 #OtherCostType.delete_all
 #FasterCSV.foreach("db/seed_files/other_cost_types.csv", :headers=>true) do |row|
 #  c=nil #ActivityCostCategory.first( :conditions => {:id =>row[:id]}) implement update later

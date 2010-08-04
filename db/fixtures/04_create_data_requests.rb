f886bf18c130de690dd7d6a11b9f524aaf1ed50c
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-08-04 05:02:07 +0200

    update orgs file and make admin have data request populated

--- db/fixtures/04_create_data_requests.rb
+++ db/fixtures/04_create_data_requests.rb
@@ -2,4 +2,6 @@ User.stub_current_user_and_data_response
 DataRequest.delete_all # TODO remove this once we have more data requests
 DataRequest.create!(:requesting_organization => Organization.create(:name=>"Government of Rwanda"), #TODO reference GOR
   :title => "Click here to enter FY2010 Workplan and FY2009 Expenditures - due date TBD")
+admin=User.find_by_username "admin"
+admin.current_data_response = DataRequest.first.data_responses.first #since UI has no way to set this currently
 User.unstub_current_user_and_data_response

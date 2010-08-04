b65c2d405d9775f5ce37be9bcff7a24055f6e02b
Glenn Roberts <glenn.roberts@siyelo.com>
2010-08-04 19:27:17 +0200

    redirect to user guide on login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -9,7 +9,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Successfully logged in."
-      redirect_to user_dashboard_path(@user_session.record)
+      redirect_to static_page_url(:user_guide)
     else
       flash[:error] = "Wrong Username/email and password combination. If you think this message is being shown in error after multiple tries, use the form on the contact page (link below) to get help."
       render :action => :new

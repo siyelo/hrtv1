b41c55352849af32c014c576d8f095fd3d6bb5ad
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-08-19 22:58:35 +0200

    Tabs autoload for coding activities

--- app/controllers/code_assignments_controller.rb
+++ app/controllers/code_assignments_controller.rb
@@ -2,53 +2,56 @@ class CodeAssignmentsController < ApplicationController
   authorize_resource
 
   def budget
-    self.load_codes
+    load_codes
     @current_codes = @activity.budget_codes
     @current_assignments = @activity.budget_codings
     @coding_type = :budget_codes
   end
 
   def budget_cost_categories
-    self.load_codes
+    load_codes
     @current_codes = @activity.budget_codes
     @current_assignments = @activity.budget_codings
     @coding_type = :budget_cost_categories
     @codes = @activity.valid_cost_category_codes
+    render :layout => false
   end
 
   def expenditure
-    self.load_codes
+    load_codes
     @current_codes = @activity.expenditure_codes
     @current_assignments = @activity.expenditure_codings
     @coding_type = :expenditure_codes
+    render :layout => false
   end
 
   def expenditure_cost_categories
-    self.load_codes
+    load_codes
     @current_codes = @activity.expenditure_codes
     @current_assignments = @activity.expenditure_codings
     @coding_type = :expenditure_cost_categories
     @codes = @activity.valid_cost_category_codes
+    render :layout => false
   end
 
   def update_budget
     @activity = Activity.available_to(current_user).find(params[:activity_id])
-    self.update_assignments("budget", budget_activity_coding_path(@activity))
+    update_assignments("budget")
   end
 
   def update_expenditure
     @activity = Activity.available_to(current_user).find(params[:activity_id])
-    self.update_assignments("expenditure", expenditure_activity_coding_path(@activity))
+    update_assignments("expenditure")
   end
 
   def update_budget_cost_categories
     @activity = Activity.available_to(current_user).find(params[:activity_id])
-    self.update_assignments("budget", budget_cost_categories_activity_coding_path(@activity))
+    update_assignments("budget")
   end
 
   def update_expenditure_cost_categories
     @activity = Activity.available_to(current_user).find(params[:activity_id])
-    self.update_assignments("expenditure", expenditure_cost_categories_activity_coding_path(@activity))
+    update_assignments("expenditure")
   end
   protected
 
@@ -58,20 +61,15 @@ class CodeAssignmentsController < ApplicationController
     @codes = @activity.valid_roots_for_code_assignment
   end
 
-  def update_assignments(coding_type, path)
+  def update_assignments(coding_type)
     #authorize! :update, @activity #GN: why is this missing
     params[:activity].delete(:code_assignment_tree) #until we figure out how to remove the checkbox inputs
 
-    respond_to do |format|
-      if @activity.update_attributes(params[:activity])
-        flash[:notice] = "Activity #{coding_type} was successfully updated."
-        format.html { redirect_to(path) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "manage" } #TODO fix path here
-        format.xml  { render :xml => @activity.errors, :status => :unprocessable_entity }
-      end
+    if @activity.update_attributes(params[:activity])
+      flash[:notice] = "Activity #{coding_type} was successfully updated."
+      redirect_to(budget_activity_coding_path(@activity))
+    else
+      render :action => "manage" #TODO fix path here
     end
   end
-
 end

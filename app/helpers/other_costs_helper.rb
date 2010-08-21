14e9200d05ff0d5971d61c94155e1aa9d27708bd
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-08-21 21:57:39 +0200

    other costs show limited project selection

--- app/helpers/other_costs_helper.rb
+++ app/helpers/other_costs_helper.rb
@@ -1,2 +1,15 @@
 module OtherCostsHelper
+  def options_for_association_conditions(association)
+    if params[:controller] == "other_costs" #this might intro a bug
+      if association.name == :projects
+          ids = Set.new
+          Project.available_to(current_user).all.each do |p|
+            ids.merge [p.id]
+          end
+          ["id in (?)", ids]
+      else
+        super
+      end
+    end
+  end
 end

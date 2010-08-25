54b7fc06936fd106729afe79c60a7021f3c97cad
Glenn Roberts <glenn.roberts@siyelo.com>
2010-08-25 17:51:39 +0200

    add after fix check

--- db/fixes/4602530_fix_data_responses.rb
+++ db/fixes/4602530_fix_data_responses.rb
@@ -5,3 +5,7 @@ Organization.all.each { |o| corrupt_orgs << o if o.data_responses.empty? }
 puts corrupt_orgs
 puts corrupt_orgs.count
 corrupt_orgs.each { |o| new_dr = d.clone; new_dr.organization_id_responder = o.id; new_dr.save!; }
+
+after_fix_orgs = []
+Organization.all.each { |o| after_fix_orgs << o if o.data_responses.empty? }
+puts after_fix_orgs.count

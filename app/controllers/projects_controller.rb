6e168b6e5c3a43ba2fed2200ef6993cca78e9140
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-09-16 22:39:10 +0200

    Moved currency under districts

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,7 +5,7 @@ class ProjectsController < ActiveScaffoldController
   before_filter :check_user_has_data_response
 
   @@shown_columns   = [:organization, :name, :description,  :budget, :spend]
-  @@create_columns  = [:name, :description, :currency, :entire_budget, :budget, :spend, :spend_q4_prev, :spend_q1, :spend_q2, :spend_q3, :spend_q4, :start_date, :end_date, :locations]
+  @@create_columns = [:name, :description, :entire_budget, :budget, :spend, :spend_q4_prev, :spend_q1, :spend_q2, :spend_q3, :spend_q4, :start_date, :end_date, :locations, :currency]
   @@upload_columns  = [:name, :description, :currency, :entire_budget, :budget, :spend, :spend_q4_prev, :spend_q1, :spend_q2, :spend_q3, :spend_q4, :start_date, :end_date ]
   def self.create_columns
     @@create_columns

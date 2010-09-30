d32ac5c23aa5958bda5568eec5f260214f0f790b
Greg Nelson <gln@georgetown.edu>
2010-09-30 19:06:18 -0400

    update message wording

--- app/helpers/data_response_helper.rb
+++ app/helpers/data_response_helper.rb
@@ -1,7 +1,7 @@
 module DataResponseHelper
   def display_warnings(warnings)
     if warnings.include?(:other_costs_missing)
-      "We noticed you did not enter any #{link_to('other costs', other_costs_path)}. Have you entered all of your expenditure and budget information?"
+      "We noticed you did not enter any #{link_to('other costs', other_costs_path)}. Have you entered all of your expenditure and budget information, including any financial information at the central level and administrative expenses?"
     elsif warnings.include?(:activities_missing)
       "We noticed you did not enter any #{link_to('activities', activities_path)}. Have you entered all of your expenditure and budget information?"
     end

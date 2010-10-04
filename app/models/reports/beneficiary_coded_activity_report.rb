d902cc2839bb57f30eb8266a4090c8f0bcee3981
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-10-04 17:30:56 +0200

    remove extraneous comments

--- app/models/reports/beneficiary_coded_activity_report.rb
+++ app/models/reports/beneficiary_coded_activity_report.rb
@@ -2,40 +2,11 @@ require 'fastercsv'
 
 class BeneficiaryCodedActivityReport < Reports::CodedActivityReport
 
-#  attr_accessible :codes, :code_ids, :get_codes_array_method, :code_id_method, :code_class
-
   def initialize # codes= nil, get_codes_array_method = nil, code_id_method = nil
     super( Beneficiary.all, :beneficiaries, :id)
   end
 
   protected
-
-#  def build_header
-#    #print header
-#    header = []
-#    header << super()
-#    codes.each do |code|
-#      header << "#{code}"
-#    end
-#    header.flatten
-#  end
-
-#  def build_rows(activity)
-#    base_row=super(activity)
-#    rows = []
-#    act_codes = activity.send(get_codes_array_method).map(&code_id_method)
-    
-#    row = []
-#    code_ids.each do |code_id|
-#      if act_codes.include?(code_id)
-#        column_value = value_for_code_column activity, code_id
-#      else
-#        row << " "
-#      end
-#    end
-#    rows = (base_row + row).flatten
-#    rows
-#  end
 
   def value_for_code_column activity, code_id
     "yes"

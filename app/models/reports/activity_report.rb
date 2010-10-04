b5f5bcecf27b2f9c3004ccea0428a547514c36bc
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-10-04 16:36:53 +0200

    activity report for descendants to take it

--- app/models/reports/activity_report.rb
+++ app/models/reports/activity_report.rb
@@ -14,59 +14,63 @@ class Reports::ActivityReport
 
     #add to cols only when you are doing a row join, not column join
     #dont do chaining yet, just one set of codes
+    # TODO add beneficiaries as first instance of codes
 
-    beneficiaries = Beneficiary.find(:all, :select => 'short_display').map(&:short_display).sort
-
-    @csv_string = FasterCSV.generate do |csv|
-      csv << build_header(beneficiaries, codes)
-
-      #print data
-      Activity.all.each do |a|
-        row = build_row(a, beneficiaries)
-        #print out a row for each project
-        if a.projects.empty?
-          row.unshift(" ")
-          csv << row.flatten
-        else
-          a.projects.each do |proj|
-            proj_row = row.dup
-            proj_row.unshift("#{h proj.name}")
-            csv << proj_row.flatten
-          end
-        end
-      end
-    end
   end
 
   def csv
+    unless @csv_string
+      @csv_string = FasterCSV.generate do |csv|
+        csv << build_header(beneficiaries, codes)
+        #print data
+        Activity.all.each do |a|
+          rows = build_rows(a)
+          add_rows_to_csv rows, csv
+          #print out a row for each project
+        end
+      end
+    end
     @csv_string
   end
 
   protected
 
-  def build_header(beneficiaries, codes)
+  def build_header
     #print header
     header = []
     header << [ "project", "org.name", "org.type", "activity.name", "activity.description" ]
-    beneficiaries.each do |ben|
-      header << "#{ben}"
-    end
     header << ["activity.text_for_beneficiaries", "activity.text_for_targets", "activity.target", "activity.budget", "activity.spend", "currency","activity.start", "activity.end", "activity.provider"]
     header.flatten
   end
 
-  def build_row(activity, beneficiaries)
+  def add_rows_to_csv rows, csv
+    if rows.first.class == Array
+      rows.each {|r| add_rows_to_csv r, csv}
+    else
+      csv << rows
+    end
+  end
+
+  def build_rows(activity)
+    rows=[]
     org        = activity.data_response.responding_organization
-    act_benefs = activity.beneficiaries.map(&:to_s)
 
     row = []
     row << [ "#{h org.name}", "#{org.type}", "#{h activity.name}", "#{h activity.description}" ]
-    beneficiaries.each do |ben|
-      row << (act_benefs.include?(ben) ? "yes" : " " )
-    end
     row << ["#{h activity.text_for_beneficiaries}", "#{h activity.text_for_targets}", "#{activity.target}", "#{activity.budget}", "#{activity.spend}", "#{activity.data_response.currency}",  "#{activity.start}", "#{activity.end}" ]
     row << (activity.provider.nil? ? " " : "#{h activity.provider.name}" )
     row.flatten
+    if activity.projects.empty?
+      row.unshift(" ")
+      rows = [ row.flatten ]
+    else
+      activity.projects.each do |proj|
+        proj_row = row.dup
+        proj_row.unshift("#{h proj.name}")
+        rows << proj_row.flatten
+      end
+    end
+    rows
   end
 
 end

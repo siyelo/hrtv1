71da82bec76798400319715ddee3b2d53eadc0df
Gregory Nelson <gregory_nelson@abtassoc.com>
2010-10-06 01:45:57 +0200

    script ready for testing

--- db/fixes/20101006_add_spend_codings_for_those_w_budget_codings_but_no_spend.rb
+++ db/fixes/20101006_add_spend_codings_for_those_w_budget_codings_but_no_spend.rb
@@ -1,3 +1,5 @@
+
+# use spend codings for budget if they are missing
 to_move_to_if_missing = { CodingBudget => CodingSpend,
   CodingBudgetCostCategorization => CodingSpendCostCategorization,
   CodingBudgetDistrict => CodingSpendDistrict}
@@ -7,13 +9,33 @@ Activity.all.each do |a|
     to_move_to_if_missing.each do |from, to|
       coding = from.with_activity(a)
       to_coding = to.with_activity(a)
-      if to_coding.empty?
-
+      if to_coding.empty? && a.spend && a.spend > 0
+        CodeAssignment.copy_coding_from_budget_to_spend coding, to
       end
     end
   end
-end
 
 # now save sub activity codings, which will take their parent's
 # expenditure codings made from the above, if they have a spend
+# and budget ones as well
+Activity.all.each do |a|
+    to_move_to_if_missing.each do |from, to| #reuse since it has all the coding classes in it
+      coding = from.with_activity(a)
+      to_coding = to.with_activity(a)
+      [coding, to_coding].each do |coding|
+        coding.each do |ca|
+          puts "error on activity #{a.id} for #{ca.code_id} #{ca.class}" unless ca.save
+        end
+      end
+end
 
+# now, save the hssp2 code_assignments
+
+Activity.all.each do |a|
+  [:budget_stratprog_coding,:spend_stratprog_coding,
+    :budget_stratobj_coding,:spend_stratobj_coding].each do |coding|
+    coding.each do |ca|
+      puts "error on activity #{a.id} for #{ca.code_id} #{ca.class}" unless ca.save
+    end
+  end
+end

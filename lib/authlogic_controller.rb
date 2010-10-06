a11c631ca131f405ff794de566f018287a1cca86
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-10-06 14:52:58 +0200

    Return current_user for current_user

--- lib/authlogic_controller.rb
+++ lib/authlogic_controller.rb
@@ -18,6 +18,7 @@ class AuthlogicController < ActionController::Base
     return @current_user if defined?(@current_user)
     @current_user ||= current_user_session && current_user_session.record
     session[:username] = @current_user.username if @current_user
+    @current_user
   end
 
   def require_user

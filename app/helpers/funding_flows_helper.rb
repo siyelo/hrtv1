1a93f0e4943bdd142c70c3d0fe0df4afa4370aab
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-10-07 13:48:05 +0200

    Fixed cucumber feature for funding sources

--- app/helpers/funding_flows_helper.rb
+++ app/helpers/funding_flows_helper.rb
@@ -46,4 +46,8 @@ module FundingFlowsHelper
     number_to_currency(record.spend, :unit => '', :separator => ".", :delimiter => ",")
   end
 
+  def budget_column record
+    number_to_currency(record.budget, :unit => '', :separator => ".", :delimiter => ",")
+  end
+
 end

3444746748b2f380b5e93c7e25046fc706d7e19c
Glenn Roberts <glenn.roberts@siyelo.com>
2010-10-15 11:15:55 +0200

    add show action

--- spec/controllers/admin/data_responses_controller_spec.rb
+++ spec/controllers/admin/data_responses_controller_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 
 describe Admin::DataResponsesController do
   describe "Routing shortcuts should map" do
-    it "GET with /admin/data_responses" do
+    it "GET (index) with /admin/data_responses" do
       params_from(:get, '/admin/data_responses').should == {:controller => "admin/data_responses", :action => "index"}
+    end
+    it "GET (show) with /admin/data_responses/1" do
+      params_from(:get, '/admin/data_responses/1').should == {:controller => "admin/data_responses", :id => "1", :action => "show"}
     end
     it "DELETE with /admin/data_responses/1" do
       params_from(:delete, "/admin/data_responses/1").should == {:controller => "admin/data_responses", :id => "1", :action => "destroy"}

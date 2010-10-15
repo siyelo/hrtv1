67c94e96d7d907261b75e2e7f1c371218bf9adc3
Glenn Roberts <glenn.roberts@siyelo.com>
2010-10-15 11:06:17 +0200

    add routing specs

--- spec/controllers/admin/data_responses_controller_spec.rb
+++ spec/controllers/admin/data_responses_controller_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe Admin::DataResponsesController do
-
-  #Delete this example and add some real ones
-  it "should use Admin::DataResponsesController" do
-    controller.should be_an_instance_of(Admin::DataResponsesController)
+  describe "Routing shortcuts should map" do
+    it "GET with /admin/data_responses" do
+      params_from(:get, '/admin/data_responses').should == {:controller => "admin/data_responses", :action => "index"}
+    end
+    it "DELETE with /admin/data_responses/1" do
+      params_from(:delete, "/admin/data_responses/1").should == {:controller => "admin/data_responses", :id => "1", :action => "destroy"}
+    end
   end
-
 end

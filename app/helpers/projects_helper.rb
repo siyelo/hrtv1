859d33b45e3703acb88e62db82f3af7e63134389
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-10-18 13:38:48 +0200

    Fixed bug with end_date in projects

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -6,6 +6,6 @@ module ProjectsHelper
   end
 
   def end_date_form_column(column, options)
-    text_field :record, :start_date, options.merge({:class => "date_picker"})
+    text_field :record, :end_date, options.merge({:class => "date_picker"})
   end
 end

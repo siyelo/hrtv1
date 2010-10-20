023f85329c346f57e40961657e27bd3c4193fb4c
Glenn Roberts <glenn.roberts@siyelo.com>
2010-10-20 18:42:49 +0200

    shorten display of callouts on pie charts

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -13,18 +13,19 @@ class ChartsController < ApplicationController
 
   private
 
-  # csv format: title, value, ?,?, description
+  # csv format for AM pie chart:
+  # title, value, ?, ?, ?, description
   def get_csv_string(records)
     other = 0
     csv_string = FasterCSV.generate do |csv|
       records.each_with_index do |record, index|
         if index < 10
-          csv << [first_n_words(h(record.name), 3), record.value.to_f, nil, nil, h(record.name) ]
+          csv << [first_n_words(h(record.name), 3), record.value.to_f, nil, nil, nil, h(record.name) ]
         else
           other += record.value.to_f
         end
       end
-      csv << ['Other', other, nil, nil, 'Other']
+      csv << ['Other', other, nil, nil, nil, 'Other']
     end
     csv_string
   end
@@ -40,7 +41,7 @@ class ChartsController < ApplicationController
   end
 
   def first_n_words(string, n)
-    string.split(' ').slice(0,n).join(' ').join('...')
+    string.split(' ').slice(0,n).join(' ') + '...'
   end
 
 end

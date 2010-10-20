011cfee37b531682866b7e7644f1cb85ff339c7e
Glenn Roberts <glenn.roberts@siyelo.com>
2010-10-20 15:59:48 +0200

    change structure of project summary

--- app/controllers/admin/data_responses_controller.rb
+++ app/controllers/admin/data_responses_controller.rb
@@ -13,12 +13,15 @@ class Admin::DataResponsesController < ApplicationController
     @projects = @data_response.projects.find(:all, :order => "name ASC")
     @code_roots = Code.for_activities.roots
     @cost_cat_roots = CostCategory.roots
+    #@activities         = @data_response.activities.roots
+    #@other_cost_activities   = @data_response.activities.with_type("OtherCost")
+    #@uncoded_activities     = @activities.reject{ |a| a.classified || (a.budget_classified? && !a.spend_classified?)  }
   end
 
   def destroy
     @data_response = DataResponse.find(params[:id])
     @data_response.destroy if @data_response.empty?
-      
+
     respond_to do |format|
       format.html do
         flash[:notice] = "Data response was successfully deleted."

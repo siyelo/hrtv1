67c57ac305a036f45afe324e78ee1be1fb33994c
Glenn Roberts <glenn.roberts@siyelo.com>
2010-10-21 23:29:17 +0200

    move closer...

--- app/models/nsp.rb
+++ app/models/nsp.rb
@@ -34,6 +34,11 @@ class Nsp < Code
     ]
   end
 
+  # Returns an array of all parents
+  def nsp_ancestors
+    without_self self_and_nsp_ancestors
+  end
+
   def self.nsp_roots_with_level
     # NSP 'roots' are embedded in other hierarchies, so we override the default awesome_nested_set :roots
     Nsp.roots.each do |nsp_root|
@@ -45,23 +50,32 @@ class Nsp < Code
     end
   end
 
+  def self.nsp_leaves_with_level
+    # NSP 'roots' are embedded in other hierarchies, so we override the default awesome_nested_set :roots
+    Nsp.leaves.each do |nsp|
+       puts "\n\n#{nsp.external_id}: #{nsp.short_display.first(20) + '...'}\n"
+      # each_with_level() is faster than level()
+      Nsp.each_with_level(nsp.self_and_nsp_ancestors.reverse) do |code, level|
+        puts "  #{level}, #{code.external_id}, #{code.short_display.first(20) + '...'} " #, #{o.code_assignments } " #.with_activities(1443)}\n"
+      end
+    end
+  end
+
   # DataResponse.find(6533).activities
-  def self.nsp_leaves_with_level(activities)
+  def self.activity_report(activities)
     csv = []
     Nsp.leaves.each do |nsp_node|
-
       Nsp.each_with_level(nsp_node.self_and_nsp_ancestors.reverse) do |code, level| # each_with_level() is faster than level()
-
         #TODO - make sure always prepending the right nr of columns
         parent_nodes = []
-        Nsp.each_with_level(code.self_and_nsp_ancestors) do |parent, level| # each_with_level() is faster than level()
+        Nsp.each_with_level(code.nsp_ancestors) do |parent, level| # each_with_level() is faster than level()
           parent_nodes << "#{parent.external_id}"
         end
 
         code.code_assignments.with_activities(activities).each do |assignment|
           row = []
-          row << "#{code.level}"
           row << "#{code.external_id}"
+          row << "#{code.level}"
           row << "#{code.short_display.first(20) + '...'}"
           row << "#{assignment.type}"
           row << "#{assignment.amount}"

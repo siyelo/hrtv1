533f53d335c324af2b7b15ce351b767552b0273e
Glenn Roberts <glenn.roberts@siyelo.com>
2010-10-21 21:37:42 +0200

    add some informational methods to Nsp codes

--- app/models/nsp.rb
+++ app/models/nsp.rb
@@ -22,10 +22,40 @@
 #
 
 class Nsp < Code
+  NSP_TYPE = 'Nsp'
 
-  named_scope :roots, :joins => "INNER JOIN codes AS parents ON codes.parent_id = parents.id", 
-              :conditions => "codes.type = 'Nsp' AND parents.type != 'Nsp'"
-  named_scope :children, :joins => "INNER JOIN codes children ON codes.id = children.parent_id",
-              :conditions => "codes.type = 'Nsp' AND children.type != 'Nsp'"
+  named_scope :roots, :joins => "INNER JOIN codes AS parents ON codes.parent_id = parents.id",
+              :conditions => { "codes.type = ? AND parents.type != ?", [NSP_TYPE, NSP_TYPE] }
+
+  # Returns the array of all parents and self
+  def self_and_nsp_ancestors
+    nested_set_scope.scoped :conditions => [
+      "type = ? AND #{self.class.quoted_table_name}.#{quoted_left_column_name} <= ? AND #{self.class.quoted_table_name}.#{quoted_right_column_name} >= ?", NSP_TYPE, left, right
+    ]
+  end
+
+  def self.nsp_roots_with_level
+    # NSP 'roots' are embedded in other hierarchies, so we override the default awesome_nested_set :roots
+    Nsp.roots.each do |nsp_root|
+       puts "\n\n#{nsp_root.external_id}: #{nsp_root.short_display.first(20) + '...'}\n"
+      # each_with_level() is faster than level()
+      Nsp.each_with_level(nsp_root.self_and_descendants) do |o, level|
+        puts "  #{level}, #{o.external_id}, #{o.short_display.first(20) + '...'}\n"
+      end
+    end
+  end
+
+  def self.nsp_leaves_with_level
+    # As above, from the bottom-up
+    Nsp.leaves.each do |nsp_node|
+      puts "\n\n#{nsp_node.external_id}: #{nsp_node.short_display.first(20) + '...'}\n"
+      # each_with_level() is faster than level()
+      Nsp.each_with_level(nsp_node.self_and_nsp_ancestors) do |o, level|
+        puts "  #{level}, #{o.external_id}, #{o.short_display.first(20) + '...'}\n"
+      end
+    end
+  end
 
 end
+
+

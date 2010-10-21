a50f47e116d25a1e52024299366179c482d8bf52
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-10-21 10:35:41 +0200

    Moved currencies migration to first migration

--- db/schema.rb
+++ db/schema.rb
@@ -17,41 +17,41 @@ ActiveRecord::Schema.define(:version => 20101020123839) do
   end
 
   create_table "activities", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "provider_id"
-    t.text     "description"
-    t.string   "type"
-    t.decimal  "budget"
-    t.decimal  "spend_q1"
-    t.decimal  "spend_q2"
-    t.decimal  "spend_q3"
-    t.decimal  "spend_q4"
-    t.date     "start"
-    t.date     "end"
-    t.decimal  "spend"
-    t.text     "text_for_provider"
-    t.text     "text_for_targets"
-    t.text     "text_for_beneficiaries"
-    t.decimal  "spend_q4_prev"
-    t.integer  "data_response_id"
-    t.integer  "activity_id"
-    t.decimal  "budget_percentage"
-    t.decimal  "spend_percentage"
-    t.boolean  "approved"
-    t.decimal  "CodingBudget_amount"
-    t.decimal  "CodingBudgetCostCategorization_amount"
-    t.decimal  "CodingBudgetDistrict_amount"
-    t.decimal  "CodingSpend_amount"
-    t.decimal  "CodingSpendCostCategorization_amount"
-    t.decimal  "CodingSpendDistrict_amount"
-    t.boolean  "use_budget_codings_for_spend",          :default => false
-    t.decimal  "budget_q1"
-    t.decimal  "budget_q2"
-    t.decimal  "budget_q3"
-    t.decimal  "budget_q4"
-    t.decimal  "budget_q4_prev"
+    t.string    "name"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.integer   "provider_id"
+    t.text      "description"
+    t.string    "type"
+    t.decimal   "budget"
+    t.decimal   "spend_q1"
+    t.decimal   "spend_q2"
+    t.decimal   "spend_q3"
+    t.decimal   "spend_q4"
+    t.date      "start"
+    t.date      "end"
+    t.decimal   "spend"
+    t.text      "text_for_provider"
+    t.text      "text_for_targets"
+    t.text      "text_for_beneficiaries"
+    t.decimal   "spend_q4_prev"
+    t.integer   "data_response_id"
+    t.integer   "activity_id"
+    t.decimal   "budget_percentage"
+    t.decimal   "spend_percentage"
+    t.boolean   "approved"
+    t.decimal   "CodingBudget_amount",                   :default => 0.0
+    t.decimal   "CodingBudgetCostCategorization_amount", :default => 0.0
+    t.decimal   "CodingBudgetDistrict_amount",           :default => 0.0
+    t.decimal   "CodingSpend_amount",                    :default => 0.0
+    t.decimal   "CodingSpendCostCategorization_amount",  :default => 0.0
+    t.decimal   "CodingSpendDistrict_amount",            :default => 0.0
+    t.boolean   "use_budget_codings_for_spend",          :default => false
+    t.decimal   "budget_q1"
+    t.decimal   "budget_q2"
+    t.decimal   "budget_q3"
+    t.decimal   "budget_q4"
+    t.decimal   "budget_q4_prev"
   end
 
   create_table "activities_beneficiaries", :id => false, :force => true do |t|

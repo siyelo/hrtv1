bf246733efe1fd105ad80b6856e84b7e3825d86b
Glenn Roberts <glenn.roberts@siyelo.com>
2010-10-21 15:27:17 +0200

    fix bug

--- app/controllers/reporter/data_responses_controller.rb
+++ app/controllers/reporter/data_responses_controller.rb
@@ -4,6 +4,7 @@ class Reporter::DataResponsesController < ApplicationController
   def show
     @data_response = current_user.data_responses.find(params[:id])
     @projects = @data_response.projects.find(:all, :order => "name ASC")
+    @activities_without_projects = @data_response.activities.roots.without_a_project
     @code_roots = Code.for_activities.roots
     @cost_cat_roots = CostCategory.roots
     @policy_maker = true #view helper

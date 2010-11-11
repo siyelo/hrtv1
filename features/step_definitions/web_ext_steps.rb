945b54ec2caabf9236e80014a9e420da41798d2d
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-11-11 13:07:46 +0100

    Organization merge improvements
    * Added ajax functionality
    * Added option to delete organization
    
    PT: https://www.pivotaltracker.com/story/show/6253977

--- features/step_definitions/web_ext_steps.rb
+++ features/step_definitions/web_ext_steps.rb
@@ -12,3 +12,38 @@ When /^(.*) within ([^:"]+):$/ do |step, scope, table_or_string|
     When "#{step}:", table_or_string
   end
 end
+
+When /^I will confirm a js popup$/ do
+  page.evaluate_script('window.confirm = function() { return true; }')
+end
+
+
+Then /^the cached field "([^"]*)" should contain "([^"]*)"$/ do |selector, value|
+  find(selector).value.should == value
+end
+
+Then /^the cached field "([^"]*)" within "([^"]*)" should contain "([^"]*)"$/ do |field, selector, value|
+  within(selector) do
+    find(field).value.should == value
+  end
+end
+
+class Capybara::XPath
+  class << self
+    def element(locator)
+      append("//*[normalize-space(text())=#{s(locator)}]")
+    end
+  end
+end
+
+When /^I click element "([^"]*)"$/ do |selector|
+  find(selector).click
+end
+
+Then /^the "([^"]*)" text should be "([^"]*)"$/ do |label, value|
+  find_field(label).text.should == value
+end
+
+Then /^the "([^"]*)" text should not be "([^"]*)"$/ do |label, value|
+  find_field(label).text.should_not == value
+end

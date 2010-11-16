f0fdc22864bfc2a0f50c30c3f5a1fd0d06c86857
Glenn Roberts <glenn.roberts@siyelo.com>
2010-11-16 15:18:46 +0200

    update schema after merge

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,8 @@
 ActiveRecord::Schema.define(:version => 20101111123715) do
 
   create_table "abilities", :force => true do |t|
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "activities", :force => true do |t|
@@ -99,33 +99,32 @@ ActiveRecord::Schema.define(:version => 20101111123715) do
   add_index "code_assignments", ["code_id"], :name => "index_code_assignments_on_code_id"
 
   create_table "codes", :force => true do |t|
-    t.integer   "parent_id"
-    t.integer   "lft"
-    t.integer   "rgt"
-    t.string    "short_display"
-    t.string    "long_display"
-    t.text      "description"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.date      "start_date"
-    t.date      "end_date"
-    t.integer   "replacement_code_id"
-    t.string    "type"
-    t.string    "external_id"
-    t.string    "hssp2_stratprog_val"
-    t.string    "hssp2_stratobj_val"
-    t.string    "official_name"
-    t.decimal   "target_amount",       :default => 0.0
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.string   "short_display"
+    t.string   "long_display"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.integer  "replacement_code_id"
+    t.string   "type"
+    t.string   "external_id"
+    t.string   "hssp2_stratprog_val"
+    t.string   "hssp2_stratobj_val"
+    t.string   "official_name"
   end
 
   create_table "comments", :force => true do |t|
-    t.string    "title",            :limit => 50, :default => ""
-    t.text      "comment",                        :default => ""
-    t.integer   "commentable_id"
-    t.string    "commentable_type"
-    t.integer   "user_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "title",            :limit => 50, :default => ""
+    t.text     "comment",                        :default => ""
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
@@ -149,12 +148,12 @@ ActiveRecord::Schema.define(:version => 20101111123715) do
   add_index "data_elements", ["data_response_id"], :name => "index_data_elements_on_data_response_id"
 
   create_table "data_requests", :force => true do |t|
-    t.integer   "organization_id_requester"
-    t.string    "title"
-    t.boolean   "complete",                  :default => false
-    t.boolean   "pending_review",            :default => false
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.integer  "organization_id_requester"
+    t.string   "title"
+    t.boolean  "complete",                  :default => false
+    t.boolean  "pending_review",            :default => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "data_responses", :force => true do |t|
@@ -185,35 +184,35 @@ ActiveRecord::Schema.define(:version => 20101111123715) do
   add_index "data_responses", ["organization_id_responder"], :name => "index_data_responses_on_organization_id_responder"
 
   create_table "field_helps", :force => true do |t|
-    t.string    "attribute_name"
-    t.string    "short"
-    t.text      "long"
-    t.integer   "model_help_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "attribute_name"
+    t.string   "short"
+    t.text     "long"
+    t.integer  "model_help_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "funding_flows", :force => true do |t|
-    t.integer   "organization_id_from"
-    t.integer   "organization_id_to"
-    t.integer   "project_id"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.decimal   "budget"
-    t.decimal   "spend_q1"
-    t.decimal   "spend_q2"
-    t.decimal   "spend_q3"
-    t.decimal   "spend_q4"
-    t.text      "organization_text"
-    t.integer   "self_provider_flag",   :default => 0
-    t.decimal   "spend"
-    t.decimal   "spend_q4_prev"
-    t.integer   "data_response_id"
-    t.decimal   "budget_q1"
-    t.decimal   "budget_q2"
-    t.decimal   "budget_q3"
-    t.decimal   "budget_q4"
-    t.decimal   "budget_q4_prev"
+    t.integer  "organization_id_from"
+    t.integer  "organization_id_to"
+    t.integer  "project_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.decimal  "budget"
+    t.decimal  "spend_q1"
+    t.decimal  "spend_q2"
+    t.decimal  "spend_q3"
+    t.decimal  "spend_q4"
+    t.text     "organization_text"
+    t.integer  "self_provider_flag",   :default => 0
+    t.decimal  "spend"
+    t.decimal  "spend_q4_prev"
+    t.integer  "data_response_id"
+    t.decimal  "budget_q1"
+    t.decimal  "budget_q2"
+    t.decimal  "budget_q3"
+    t.decimal  "budget_q4"
+    t.decimal  "budget_q4_prev"
   end
 
   add_index "funding_flows", ["data_response_id"], :name => "index_funding_flows_on_data_response_id"
@@ -221,17 +220,17 @@ ActiveRecord::Schema.define(:version => 20101111123715) do
   add_index "funding_flows", ["self_provider_flag"], :name => "index_funding_flows_on_self_provider_flag"
 
   create_table "help_requests", :force => true do |t|
-    t.string    "email"
-    t.text      "message"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "email"
+    t.text     "message"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "locations", :force => true do |t|
-    t.string    "name"
-    t.string    "type"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "name"
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "locations_organizations", :id => false, :force => true do |t|
@@ -245,72 +244,93 @@ ActiveRecord::Schema.define(:version => 20101111123715) do
   end
 
   create_table "model_helps", :force => true do |t|
-    t.string    "model_name"
-    t.string    "short"
-    t.text      "long"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "model_name"
+    t.string   "short"
+    t.text     "long"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "organizations", :force => true do |t|
-    t.string    "name"
-    t.string    "type"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.string    "raw_type"
-    t.string    "fosaid"
+    t.string   "name"
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "raw_type"
+    t.string   "fosaid"
   end
 
   create_table "projects", :force => true do |t|
-    t.string    "name"
-    t.text      "description"
-    t.date      "start_date"
-    t.date      "end_date"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.decimal   "budget"
-    t.decimal   "spend"
-    t.decimal   "entire_budget"
-    t.string    "currency"
-    t.decimal   "spend_q1"
-    t.decimal   "spend_q2"
-    t.decimal   "spend_q3"
-    t.decimal   "spend_q4"
-    t.decimal   "spend_q4_prev"
-    t.integer   "data_response_id"
-    t.decimal   "budget_q1"
-    t.decimal   "budget_q2"
-    t.decimal   "budget_q3"
-    t.decimal   "budget_q4"
-    t.decimal   "budget_q4_prev"
-    t.integer   "comments_count",   :default => 0
+    t.string   "name"
+    t.text     "description"
+    t.date     "start_date"
+    t.date     "end_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.decimal  "budget"
+    t.decimal  "spend"
+    t.decimal  "entire_budget"
+    t.string   "currency"
+    t.decimal  "spend_q1"
+    t.decimal  "spend_q2"
+    t.decimal  "spend_q3"
+    t.decimal  "spend_q4"
+    t.decimal  "spend_q4_prev"
+    t.integer  "data_response_id"
+    t.decimal  "budget_q1"
+    t.decimal  "budget_q2"
+    t.decimal  "budget_q3"
+    t.decimal  "budget_q4"
+    t.decimal  "budget_q4_prev"
+    t.integer  "comments_count",   :default => 0
   end
 
   add_index "projects", ["data_response_id"], :name => "index_projects_on_data_response_id"
 
   create_table "sessions", :force => true do |t|
-    t.string    "session_id", :null => false
-    t.text      "data"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
-    t.string    "username"
-    t.string    "email"
-    t.string    "crypted_password"
-    t.string    "password_salt"
-    t.string    "persistence_token"
-    t.timestamp "created_at"
-    t.timestamp "updated_at"
-    t.integer   "roles_mask"
-    t.integer   "organization_id"
-    t.integer   "data_response_id_current"
-    t.text      "text_for_organization"
-    t.string    "full_name"
+    t.string   "username"
+    t.string   "email"
+    t.string   "crypted_password"
+    t.string   "password_salt"
+    t.string   "persistence_token"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "roles_mask"
+    t.integer  "organization_id"
+    t.integer  "data_response_id_current"
+    t.text     "text_for_organization"
+    t.string   "full_name"
+  end
+
+  create_table "virtual_code_codes", :force => true do |t|
+    t.integer  "code_id"
+    t.integer  "virtual_code_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "virtual_code_groups", :force => true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "virtual_codes", :force => true do |t|
+    t.string   "title"
+    t.integer  "virtual_code_group_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

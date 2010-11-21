633ebd0a9fabdeeca7b4e5b5b2b743f426a40ef1
Greg Nelson <gln@georgetown.edu>
2010-11-21 15:42:30 -0500

    new currencies

--- db/seed_files/currencies.rb
+++ db/seed_files/currencies.rb
@@ -1,3 +1,6 @@
+Currency.delete_all
 Currency.create :toRWF => 580, :symbol => "USD", :name => "dollar"
 Currency.create :toRWF => 800, :symbol => "EUR", :name => "euro"
 Currency.create :toRWF => 1, :symbol => "RWF", :name => "rwandan franc"
+Currency.create :toRWF => 928, :symbol => "GBP", :name => "british pound"
+Currency.create :toRWF => 585, :symbol => "CHF", :name => "swiss franc"

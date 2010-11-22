8697d9a657fd487462c04677b7469896de54d8e8
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-11-22 11:01:19 +0100

    Disable spoofing check
    PT: https://www.pivotaltracker.com/story/show/6509545

--- config/environment.rb
+++ config/environment.rb
@@ -36,9 +36,13 @@ Rails::Initializer.run do |config|
 
   config.time_zone = 'UTC'
 
-  #tell rails to load files from all subfolders in app/models/
+  # tell rails to load files from all subfolders in app/models/
   config.load_paths += Dir["#{RAILS_ROOT}/app/models/*"].find_all { |f| File.stat(f).directory? }
 
+  # disable spoofing check
+  # http://pivotallabs.com/users/jay/blog/articles/1216-standup-4-7-2010-disabling-rails-ip-spoofing-safeguard
+  # PT: https://www.pivotaltracker.com/story/show/6509545
+  config.action_controller.ip_spoofing_check = false
 end
 
 require 'array_extensions'

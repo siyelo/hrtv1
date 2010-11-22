7eca9a1012b376fb8b3cf0c1dbef612449cfe82d
Greg Nelson <gln@georgetown.edu>
2010-11-21 19:28:47 -0500

    first draft of facility report

---
+++ app/models/reports/map_facilities_by_partner.rb
@@ -0,0 +1,152 @@
+require 'fastercsv'
+
+class Reports::MapFacilitiesByPartner < Reports::CodedActivityReport
+  include Reports::Helpers
+ 
+
+# location => partner, amount 
+  def initialize(activities, report_type)
+    @codes_to_include = []
+  #  [9020101, 90207].each do |e|
+  #    @codes_to_include << Nsp.find_by_external_id(e)
+    facilities_to_include = [Organization.find_by_name("") #Organization.all(:conditions => ["fosaid is not null"])
+      #[Organization.find_by_name("EGPAF"), Organization.find_by_name("CCHIPs")] #Org.all :joins => :provider_for
+    facilities_to_include.each do |e|
+      @codes_to_include << e #if e.activities.count > 0
+    end
+    @districts_hash = {}
+    facilities_to_include.all.each do |l|
+      @districts_hash[l] = {}
+      @districts_hash[l][:total] = 0
+      @districts_hash[l][:partner_amt] = {} # partner => amt
+    end
+#    @district_proportions_hash = {} # activity => {location => proportion}
+    @csv_string = FasterCSV.generate do |csv|
+      @activities = activities
+      @report_type = report_type.constantize
+      @codes_to_include.each do |c|
+        set_district_hash_for_code c
+      end
+
+      csv << header()
+      #raise @districts_hash.to_yaml
+      Location.all.each do |l|
+        row csv, l, @activities, @report_type
+      end
+    end
+  end
+
+  def set_district_hash_for_code code
+    #code is facility
+    facility = code
+    #NOTE need to convert currencies and dynamic calcs not being used here 
+    
+    #cas = @report_type.with_activities(code.provider_for.only_simple.map(&:id))#.with_code_id(code.id)
+    # or
+    facility.provider_for.canonical.each do |act|
+      #act = Activity.find(1107) 
+      amt = act.budget if @report_type == CodingBudgetDistrict
+      amt = act.spend if @report_type == CodingSpendDistrict
+      amt = 0 if amt.nil?
+      amt = amt * act.toRWF
+      loc = facility
+      partner = act.data_response.responding_organization
+      @districts_hash[loc][:total] += amt
+      unless @districts_hash[loc][:partner_amt][partner].nil?
+        @districts_hash[loc][:partner_amt][partner] += amt
+      else
+        @districts_hash[loc][:partner_amt][partner] = amt unless amt == 0
+      end
+    end
+  end
+
+  def row(csv, loc, activities, report_type)
+    #hierarchy = code_hierarchy(code)
+    row = []
+    row << loc.to_s.upcase
+    row << n2c(@districts_hash[loc].delete(:total)) #remove key
+    code_to_amt = @districts_hash[loc][:partner_amt]
+    unless code_to_amt.size == 0
+      sorted_code_amt = code_to_amt.sort{|a,b| b[1]<=>a[1]} #sort by value, desc
+      # show top one
+      top = sorted_code_amt.first
+      row << top[0].to_s
+      row << n2c(top[1])
+   
+      # show full list
+      row << sorted_code_amt.collect{|e| "#{e[0].to_s}(#{n2c(e[1])})"}.join(",")
+  
+      sorted_code_amt.shift #dont show top again
+      # show in cols
+      # after sorting by amt
+      sorted_code_amt.each do |e|
+        row << e[0].to_s
+        row << n2c(e[1])
+      end
+    end
+    csv <<  row
+  end
+
+  def header()
+    row = []
+    row << "District"
+    row << "Total Budget"
+    row << "1st Development Partner by Amount"
+    row << "Amount"
+    row << "All DP's"
+    (@districts_hash.collect{|k,v| v[:partner_amt]}.map(&:size).max - 1).times do |i| #for one with most partners
+      row << "#{i+2} DP by Amount"
+      row << "#{i+2} Amount"
+    end
+    row
+  end
+  def csv
+    @csv_string
+  end
+
+  def add_rows csv, code
+    add_code_summary_row(csv, code)
+    row(csv, code, @activities, @report_type) if @districts_hash.key? code
+    kids = code.children
+    kids.each do |c|
+      add_rows(csv, c)
+    end
+  end
+
+  def add_code_summary_row csv, code
+#    csv << "In NSP #{code.short_display} #{code.id} #{code.external_id} "
+#    csv << code.external_id.to_s
+#    total_for_code = code.sum_of_assignments_for_activities(@report_type, @activities)
+#    if total_for_code > 0
+#      csv << (code_hierarchy(code) + [nil,nil, "Total Budget - "+n2c(total_for_code)]) #put total in Q1 column
+#    end
+    #TODO merge cells[code.level:amount_column] for this code
+  end
+
+  # We've got non-report type report type hard coding here
+  # so it uses budgets
+
+
+  protected
+
+  def code_hierarchy(code)
+    # TODO merge all columns to the left and put row's value
+    # if there is more than 5 rows in the section
+    hierarchy = []
+    Code.each_with_level(code.self_and_ancestors) do |e, level| # each_with_level() is faster than level()
+      if e==code
+        hierarchy << official_name_w_sum(e)
+      else
+        hierarchy << nil
+      end
+      #hierarchy << "#{e.external_id} - #{e.sum_of_assignments_for_activities(@report_type, @activities)}"
+    end
+    (Code.deepest_nesting - hierarchy.size).times{ hierarchy << nil } #append empty columns if nested higher
+    hierarchy
+  end
+
+  def official_name_w_sum code
+    "#{code.official_name}" # - #{n2c( code.sum_of_assignments_for_activities(@report_type, @activities) )}"
+  end
+
+end

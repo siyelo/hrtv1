24210cc888176b2b854b7593fe2d456bbb9bd16a
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-11-29 16:52:20 +0100

    Remove unused tables and assignments controller
    * assignments
    * abilities
    * indicators
    * locations
    
    PT: https://www.pivotaltracker.com/story/show/6839669

--- db/migrate/20101129150541_remove_assignments_table.rb
+++ db/migrate/20101129150541_remove_assignments_table.rb
@@ -1,15 +1,12 @@
-class Assignments < ActiveRecord::Migration
-
+class RemoveAssignmentsTable < ActiveRecord::Migration
   def self.up
+    drop_table :assignments
+  end
+
+  def self.down
     create_table :assignments do |t|
       t.integer :user_id
       t.integer :role_id
     end
   end
-
-  def self.down
-    drop_table :assignments
-  end
-
 end
-

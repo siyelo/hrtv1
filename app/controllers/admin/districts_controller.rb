4fda398123e0b365c711b42125922cfe6c338620
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2010-12-14 23:15:28 +0100

    Districts listing page with optimized selects

--- app/controllers/admin/districts_controller.rb
+++ app/controllers/admin/districts_controller.rb
@@ -1,7 +1,13 @@
 class Admin::DistrictsController < Admin::BaseController
 
   def index
-    @locations = Location.all
+    @locations = Location.all_with_counters
+    @spent_codings = CodingSpendDistrict.find(:all, 
+                       :select => "code_id, SUM(cached_amount) AS total",
+                       :conditions => ["code_id IN (?)", @locations.map(&:id)], :group => 'code_id')
+    @budget_codings = CodingBudgetDistrict.find(:all, 
+                       :select => "code_id, SUM(cached_amount) AS total",
+                       :conditions => ["code_id IN (?)", @locations.map(&:id)], :group => 'code_id')
   end
 
   def show

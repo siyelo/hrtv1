b2f39f02a3f4ee00f4ab34f0c7ac95586175509f
Glenn Roberts <glenn.roberts@siyelo.com>
2011-01-12 18:48:26 +0200

    fix date format in backup script

--- db/fixes/heroku_backup.sh
+++ db/fixes/heroku_backup.sh
@@ -21,7 +21,7 @@ expect \"Are you sure you wish to continue? (y/n)? \"
 send \"y\r\"
 set results $expect_out(buffer)
 expect eof"
-echo "  db pull done at `date '+%Y-%m-%d-%I%Mhrs'`"
+echo "  db pull done at `date '+%Y-%m-%d-%H%Mhrs'`"
 
 echo "  gzipping it"
 gzip $BACKUP_DB_FILE

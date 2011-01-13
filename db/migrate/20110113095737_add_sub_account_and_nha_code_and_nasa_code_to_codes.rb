d5077e3fb23d9e00b38a1b62d19fa991638360b8
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-01-13 11:04:14 +0100

    New codes fields and new seed file
    * Added new seed file
    * Fixed scientific number errors in seed file
    * Added nasa_code, nha_code and sub_account fields to codes table

---
+++ db/migrate/20110113095737_add_sub_account_and_nha_code_and_nasa_code_to_codes.rb
@@ -0,0 +1,13 @@
+class AddSubAccountAndNhaCodeAndNasaCodeToCodes < ActiveRecord::Migration
+  def self.up
+    add_column :codes, :sub_account, :string
+    add_column :codes, :nha_code, :string
+    add_column :codes, :nasa_code, :string
+  end
+
+  def self.down
+    remove_column :codes, :nasa_code
+    remove_column :codes, :nha_code
+    remove_column :codes, :sub_account
+  end
+end

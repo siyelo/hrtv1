cfad1ca89c7c9bcddd3bf56a04e75b2921ed83c2
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-01-13 13:56:42 +0100

    Load new codes in migration

--- db/migrate/20110113095737_add_sub_account_and_nha_code_and_nasa_code_to_codes.rb
+++ db/migrate/20110113095737_add_sub_account_and_nha_code_and_nasa_code_to_codes.rb
@@ -3,6 +3,8 @@ class AddSubAccountAndNhaCodeAndNasaCodeToCodes < ActiveRecord::Migration
     add_column :codes, :sub_account, :string
     add_column :codes, :nha_code, :string
     add_column :codes, :nasa_code, :string
+
+    load 'db/seed_files/codes.rb'
   end
 
   def self.down

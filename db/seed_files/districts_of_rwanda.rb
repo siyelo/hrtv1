e4a252b6a3e5ecee60e0b86dca2111ab30ad3dd1
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-01-19 12:58:01 +0100

    Added districts seed migration

--- db/seed_files/districts_of_rwanda.rb
+++ db/seed_files/districts_of_rwanda.rb
@@ -1,16 +1,26 @@
-puts "\nloading districts"
+# Districts seed depends on existing locations in database
+# You will need to run this file after locations exist in codes table
+
+print "\nloading districts "
 
 population_col = 2
 district_col   = 7
 
 FasterCSV.foreach("db/seed_files/districts_of_rwanda.csv", :headers => true) do |row|
-  name = row[district_col].capitalize
+  begin
+    print '.'
+    name = row[district_col].capitalize
 
-  old_location          = Location.find_by_short_display(name)
-  raise "No location found for #{name}" unless old_location
+    old_location          = Location.find_by_short_display(name)
+    raise "No location found for #{name}".to_yaml unless old_location
 
-  district              = District.find_or_create_by_name(name)
-  district.population   = row[population_col]
-  district.old_location = old_location
-  district.save!
+    district              = District.find_or_create_by_name(name)
+    district.population   = row[population_col]
+    district.old_location = old_location
+    district.save!
+  rescue
+    puts "ERROR: Location is missing - ignore if you will seed the db #{$!}"
+  end
 end
+
+puts "\n\n"

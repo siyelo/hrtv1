8b2019d7c4af793c5e057f58bf7c1fade9f7d8cf
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-01-20 17:06:52 +0100

    Activities by full coding report refactoring

--- app/models/reports/activities_by_full_coding.rb
+++ app/models/reports/activities_by_full_coding.rb
@@ -1,18 +1,17 @@
 require 'fastercsv'
 
-class Reports::ActivitiesByFullCoding < Reports::CodedActivityReport
+class Reports::ActivitiesByFullCoding
   include Reports::Helpers
-  
-  def initialize(activities, report_type, show_respondent = false)
-    @show_respondent = show_respondent
+
+  def initialize(activities, report_type, show_organization = false)
+    @activities        = activities
+    @report_type       = report_type
+    @show_organization = show_organization
+    @leaves            = Code.leaves.select{|s| %w[Nsp Nha Nasa].include?(s.type.to_s)}
+
     @csv_string = FasterCSV.generate do |csv|
-      csv << header()
-      @activities = activities
-      @report_type = report_type
-      @leaves = Code.leaves.select{|s| %w[Nsp Nha Nasa].include?(s.type.to_s)}
-      Mtef.roots.reverse.each do |nsp_root|
-        add_rows csv, nsp_root
-      end
+      csv << build_header
+      Mtef.roots.reverse.each{|code| add_rows(csv, code)}
     end
   end
 
@@ -20,110 +19,108 @@ class Reports::ActivitiesByFullCoding < Reports::CodedActivityReport
     @csv_string
   end
 
-  def add_rows csv, code
+  def add_rows(csv, code)
     add_code_summary_row(csv, code)
-    row(csv, code, @activities, @report_type)
-    kids = code.children#.with_type("Nsp")
-    kids.each do |c|
-      add_rows(csv, c)
-    end
+    add_code_row(csv, code, @activities, @report_type)
+    code.children.each{|code| add_rows(csv, code)}
   end
 
-  def add_code_summary_row csv, code
+  def add_code_summary_row(csv, code)
 #    csv << "In NSP #{code.short_display} #{code.id} #{code.external_id} "
 #    csv << code.external_id.to_s
     total_for_code = code.sum_of_assignments_for_activities(@report_type, @activities)
     if total_for_code > 0
-      csv << (code_hierarchy(code) + [nil,nil, "Total Budget - "+n2c(total_for_code)]) #put total in Q1 column
+      row = []
+      add_code_hierarchy(row, code)
+      row << nil
+      row << nil
+      row << "Total Budget - " + n2c(total_for_code) #put total in Q1 column
+
+      csv << row
     end
   end
 
-  def row(csv, code, activities, report_type)
-    hierarchy = code_hierarchy(code)
-    #TODO don't show code hierarchy 
+  def add_code_row(csv, code, activities, report_type)
+    #TODO don't show code hierarchy
     # since can tell by indentation
-    cas = code.leaf_assigns_for_activities_for_code_set(report_type, @leaves, activities)
-    cas.each do |assignment|
+    code_assignments = code.leaf_assigns_for_activities_for_code_set(report_type, @leaves, activities)
+    code_assignments.each do |assignment|
       if assignment.cached_amount
         activity = assignment.activity
+
         row = []
-        row = hierarchy.clone
+        add_code_hierarchy(row, code)
         row << n2c(assignment.cached_amount)
-        if activity.name.blank?
-          unless activity.description.nil?
-            row << activity.description.chomp
-          else
-            row << nil
-          end
-        else
-          val = "#{activity.name.chomp}"
-          val += " - #{activity.description.chomp}" unless activity.description.nil?
-          row << val
-        end
+        row << activity_description(activity)
         row << activity.spend_q1 ? 'x' : nil
         row << activity.spend_q2 ? 'x' : nil
         row << activity.spend_q3 ? 'x' : nil
         row << activity.spend_q4 ? 'x' : nil
         row << activity.locations.join(' | ')
-        row << activity.data_response.responding_organization.try(:short_name) if @show_respondent
-        if assignment.activity.provider
-          row << activity.provider.try(:short_name) 
-        else
-          row << "No Implementer Specified"
-        end
+        row << activity.organization.try(:short_name) if @show_organization
+        row << get_provider_name(activity) # TODO: use provider_name(assignment.activity)
         row << activity.organizations.join(' | ')
-        unless activity.sub_activities.implemented_by_health_centers.empty?
-          row << activity.sub_activities.implemented_by_health_centers.count
-        else
-          row << nil
-        end
+        row << number_of_health_centers(activity)
         row << activity.beneficiaries.join(' | ')
         row << activity.id
-        csv <<  row
+
+        csv << row
       end
     end
   end
 
-  def header()
-    row = []
-    row << "Code"
-    (Code.deepest_nesting-1).times do |i|
-      row << "Code"
+  private
+
+    def build_header
+      row = []
+
+      Code.deepest_nesting.times{|i| row << "Code"}
+      row << "Budget"
+      row << "Activity Description"
+      row << "Q1"
+      row << "Q2"
+      row << "Q3"
+      row << "Q4"
+      row << "Districts"
+      row << "Data Source" if @show_organization
+      row << "Implementer"
+      row << "Institutions Assisted"
+      row << "# of HC's Sub-implementing"
+      row << "Beneficiaries"
+      row << "ID"
+
+      row
     end
-    row << "Budget"
-    row << "Activity Description"
-    row << "Q1"
-    row << "Q2"
-    row << "Q3"
-    row << "Q4"
-    row << "Districts"
-    row << "Data Source" if @show_respondent
-    row << "Implementer"
-    row << "Institutions Assisted"
-    row << "# of HC's Sub-implementing"
-    row << "Beneficiaries"
-    row << "ID"
-    row
-  end
 
-  protected
+    def official_name_w_sum(code)
+      code.official_name ? "#{code.official_name}" : "#{code.short_display}"
+    end
 
-  def code_hierarchy(code)
-    hierarchy = []
-    Code.each_with_level(code.self_and_ancestors) do |e, level| # each_with_level() is faster than level()
-      if e==code
-        hierarchy << official_name_w_sum(e)
+    def number_of_health_centers(activity)
+      health_centers = activity.sub_activities.implemented_by_health_centers.count
+      health_centers > 0 ? health_centers : nil
+    end
+
+    def activity_description(activity)
+      if activity.name
+        val = "#{activity.name.chomp}"
+        val += " - #{activity.description.chomp}" if activity.description
+        val
       else
-        hierarchy << nil
+        activity.description ? activity.description.chomp : nil
       end
     end
-    (Code.deepest_nesting - hierarchy.size).times{ hierarchy << nil } #append empty columns if nested higher
-    hierarchy
-  end
 
-  #TODO refactor name now that we dont show sum w it
-  def official_name_w_sum code
-    "#{code.official_name ? code.official_name : code.short_display}"
-  end
+    def get_provider_name(activity)
+      activity.provider ? activity.provider.try(:short_name) : "No Implementer Specified"
+    end
 
+    def add_code_hierarchy(row, code)
+      counter = 0
+      Code.each_with_level(code.self_and_ancestors) do |other_code, level|
+        counter += 1
+        row << (code == other_code ? official_name_w_sum(other_code) : nil)
+      end
+      (Code.deepest_nesting - counter).times{ row << nil }
+    end
 end

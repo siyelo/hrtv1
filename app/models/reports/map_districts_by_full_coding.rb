289cfa50efdab0b645a0b08732d7650fcce80f74
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-01-21 17:14:17 +0100

    Map districts by full coding report refactoring

--- app/models/reports/map_districts_by_full_coding.rb
+++ app/models/reports/map_districts_by_full_coding.rb
@@ -1,132 +1,87 @@
 require 'fastercsv'
 
-class Reports::MapDistrictsByFullCoding < Reports::CodedActivityReport
+class Reports::MapDistrictsByFullCoding
   include Reports::Helpers
 
   def initialize(activities, report_type)
-    @codes_to_include = []
-    Code.all.each do |e|
-      @codes_to_include << e if [Mtef, Nha, Nsp, Nasa].include?(e.class)
-    end
-    @districts_hash = {}
-    Location.all.each do |l|
-      @districts_hash[l] = {}
-      @districts_hash[l][:total] = 0
-      @codes_to_include.each do |c|
-        @districts_hash[l][c] = 0
-      end
-    end
+    @activities                = activities
+    @report_type               = report_type
     @district_proportions_hash = {} # activity => {location => proportion}
+    @districts_hash            = {}
+    @leaves                    = Nsp.leaves
+    @codes                     = []
+
+    Code.all.each do |code|
+      @codes << code if [Mtef, Nha, Nsp, Nasa].include?(code.class)
+    end
+
+    Location.all.each do |location|
+      @districts_hash[location] = {}
+      @districts_hash[location][:total] = 0
+      @codes.each do |code|
+        @districts_hash[location][code] = 0
+      end
+    end
+
+    @codes.each{|code| set_district_hash_for_code(code)}
+
     @csv_string = FasterCSV.generate do |csv|
-      csv << header()
-      @activities = activities
-      @report_type = report_type
-      @leaves = Nsp.leaves
-      @codes_to_include.each do |c|
-        set_district_hash_for_code c
-      end
-      Location.all.each do |l|
-        row csv, l, @activities, @report_type
-      end
+      csv << build_header
+      Location.all.each{|location| build_row(csv, location, @activities, @report_type)}
     end
   end
 
-  def set_district_hash_for_code code
-    cas = @report_type.with_activities(@activities.map(&:id)).with_code_id(code.id)
-    activities = self.cache_activities(cas)
-    activities.each do |a, h|
-      if @district_proportions_hash.key? a
-        #have cached values, so speed up these proportions
-        @district_proportions_hash[a].each do |loc, proportion|
-          @districts_hash[loc][:total] += h[:leaf_amount] * proportion
-          @districts_hash[loc][code] += h[:amount] * proportion
-        end
-      else
-        @district_proportions_hash[a] = {}
-        # We've got non-report type report type hard coding here
-        # so it uses budgets
-        a.budget_district_coding.each do |bd|
-          proportion = bd.proportion_of_activity
-          loc = bd.code
-          @district_proportions_hash[a][loc] = proportion
-          @districts_hash[loc][:total] += h[:leaf_amount] * proportion
-          @districts_hash[loc][code] += h[:amount] * proportion
-        end
-      end
-    end
-  end
-  def row(csv, loc, activities, report_type)
-    #hierarchy = code_hierarchy(code)
-    row = []
-    row << loc.to_s.upcase
-    row << n2c(@districts_hash[loc].delete(:total)) #remove key
-    code_to_amt = @districts_hash[loc]
-    @codes_to_include.each do |c|
-      if code_to_amt[c] != 0
-        row << n2c(code_to_amt[c])
-      else
-        row << nil
-      end
-    end
-    csv <<  row
-  end
-
-  def header()
-    row = []
-    row << "District"
-    row << "Total Budget"
-    @codes_to_include.each do |c|
-      row << c.official_name
-    end
-    row
-  end
   def csv
     @csv_string
   end
 
-  def add_rows csv, code
-    add_code_summary_row(csv, code)
-    row(csv, code, @activities, @report_type) if @districts_hash.key? code
-    kids = code.children
-    kids.each do |c|
-      add_rows(csv, c)
+  private
+
+    def build_header
+      row = []
+
+      row << "District"
+      row << "Total Budget"
+      @codes.each {|code| row << code.official_name}
+
+      row
     end
-  end
 
-  def add_code_summary_row csv, code
-#    csv << "In NSP #{code.short_display} #{code.id} #{code.external_id} "
-#    csv << code.external_id.to_s
-    total_for_code = code.sum_of_assignments_for_activities(@report_type, @activities)
-    if total_for_code > 0
-      csv << (code_hierarchy(code) + [nil,nil, "Total Budget - "+n2c(total_for_code)]) #put total in Q1 column
-    end
-    #TODO merge cells[code.level:amount_column] for this code
-  end
+    def build_row(csv, location, activities, report_type)
+      row = []
 
-  # We've got non-report type report type hard coding here
-  # so it uses budgets
+      row << location.to_s.upcase
+      row << n2c(@districts_hash[location].delete(:total)) #remove key
+      code_to_amt = @districts_hash[location]
 
-
-  protected
-
-  def code_hierarchy(code)
-    # TODO merge all columns to the left and put row's value
-    # if there is more than 5 rows in the section
-    hierarchy = []
-    Code.each_with_level(code.self_and_ancestors) do |e, level| # each_with_level() is faster than level()
-      if e==code
-        hierarchy << official_name_w_sum(e)
-      else
-        hierarchy << nil
+      @codes.each do |code|
+        row << (code_to_amt[code] != 0 ? n2c(code_to_amt[code]) : nil)
       end
-      #hierarchy << "#{e.external_id} - #{e.sum_of_assignments_for_activities(@report_type, @activities)}"
+
+      csv << row
     end
-    (Code.deepest_nesting - hierarchy.size).times{ hierarchy << nil } #append empty columns if nested higher
-    hierarchy
-  end
 
-  def official_name_w_sum code
-    "#{code.official_name}" # - #{n2c( code.sum_of_assignments_for_activities(@report_type, @activities) )}"
-  end
-
+    def set_district_hash_for_code(code)
+      code_assignments = @report_type.with_activities(@activities.map(&:id)).with_code_id(code.id)
+      cache_activities(code_assignments).each do |activity, amounts_hash|
+        if @district_proportions_hash.key?(activity)
+          #have cached values, so speed up these proportions
+          @district_proportions_hash[activity].each do |location, proportion|
+            @districts_hash[location][:total] += amounts_hash[:leaf_amount] * proportion
+            @districts_hash[location][code] += amounts_hash[:amount] * proportion
+          end
+        else
+          @district_proportions_hash[activity] = {}
+          # We've got non-report type report type hard coding here
+          # so it uses budgets
+          activity.budget_district_coding.each do |bd|
+            proportion = bd.proportion_of_activity
+            location = bd.code
+            @district_proportions_hash[activity][location] = proportion
+            @districts_hash[location][:total] += amounts_hash[:leaf_amount] * proportion
+            @districts_hash[location][code] += amounts_hash[:amount] * proportion
+          end
+        end
+      end
+    end
 end

ae09560ed9a5d9b3899bc3b4a01d8fa757588411
Glenn Roberts <glenn.roberts@siyelo.com>
2011-01-28 16:17:43 +0200

    change mailer name to be more friendly

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -4,7 +4,7 @@ class Notifier < ActionMailer::Base
 
   def password_reset_instructions(user)
     subject       "[Health Resource Tracker] Password Reset Instructions"
-    from          "HRT Notifier <noreply@hrtapp.com>"
+    from          "HRT Notifier <hrt-do-not-reply@hrtapp.com>"
     recipients    user.email
     sent_on       Time.now
     body          :password_reset_url => edit_password_reset_url(user.perishable_token)

73fc043b2df9433fde75c333c8f72b62534f3ffe
Greg Nelson <gln@georgetown.edu>
2011-01-28 04:12:02 -0500

    fix migration to run on empty db

--- db/migrate/20110113095736_remove_duplicate_mtef_code.rb
+++ db/migrate/20110113095736_remove_duplicate_mtef_code.rb
@@ -1,14 +1,25 @@
 class RemoveDuplicateMtefCode < ActiveRecord::Migration
   def self.up
-    puts ActiveRecord::Base.connection.execute("
-      update code_assignments
-      set code_id = 1197
-      where code_id = 1207")
-    puts Code.find("1207").delete + " deleted "
+
+    begin
+      dupe_id = 1207
+      real_id = 1197
+      dupe = Code.find(dupe_id)
+      real = Code.find(real_id)
+  
+      puts ActiveRecord::Base.connection.execute("
+        update code_assignments
+        set code_id = #{real_id}
+        where code_id = #{dupe_id}")
+  
+      puts c.delete + " deleted " if c
+    rescue ActiveRecord::RecordNotFound
+      puts "Didn't remove duplicate that wasn't found"
+    end
 
   end
 
   def self.down
-    raise "Irreversible Migration"
+    puts "Irreversible Migration that removed duplicate"
   end
 end

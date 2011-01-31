1d5224a0394edb80ca372ddc5bd2498019efda71
Greg Nelson <gln@georgetown.edu>
2011-01-31 00:36:06 -0500

    populate fields only if they exist so later migrations continue to work

--- db/seed_files/codes.rb
+++ db/seed_files/codes.rb
@@ -42,9 +42,11 @@ FasterCSV.foreach("db/seed_files/codes.csv", :headers=>true) do |row|
       c.type          = t
     end
     c.description   = row[description_col]
-    #c.sub_account   = row[sub_account_col]
-    #c.nha_code      = row[nha_code_col]
-    #c.nasa_code     = row[nasa_code_col]
+    if c.respond_to? :sub_account=
+      c.sub_account   = row[sub_account_col]
+      c.nha_code      = row[nha_code_col]
+      c.nasa_code     = row[nasa_code_col]
+    end
     c.short_display = row[short_display_col]
     c.short_display = row[class_col] unless c.short_display
     c.long_display  = row[long_display_col]

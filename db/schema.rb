72f0a1c5a9ee374077e0a45b94ea4852963cb9fc
Glenn Roberts <glenn.roberts@siyelo.com>
2011-01-31 21:11:58 +0200

    update migration, rename usd caching fix

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110131163543) do
+ActiveRecord::Schema.define(:version => 20110131180911) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -172,11 +172,11 @@ ActiveRecord::Schema.define(:version => 20110131163543) do
   add_index "data_responses", ["organization_id_responder"], :name => "index_data_responses_on_organization_id_responder"
 
   create_table "districts", :force => true do |t|
-    t.string   "name"
-    t.integer  "population"
-    t.integer  "old_location_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string    "name"
+    t.integer   "population"
+    t.integer   "old_location_id"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
   end
 
   create_table "field_helps", :force => true do |t|

d4e9a8fc692d7debad58d72357e07b88de6a5c96
Glenn Roberts <glenn.roberts@siyelo.com>
2011-02-07 16:37:15 +0200

    remove complex logic from admin reports controller

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -6,25 +6,13 @@ class Admin::ReportsController < Admin::BaseController
   end
 
   def show
-    if params[:force] == "true"
+    report = Report.find_last_by_key(params[:id])
+
+    if params[:force] == "true" || report.nil?
       report = Report.create(:key => params[:id])
-      time = report_time(Time.now.utc)
-    else
-      report = Report.find_last_by_key(params[:id])
-      if report
-        time = report_time(report.updated_at)
-      else
-        report = Report.create(:key => params[:id]) # if the report is not in database, generate it!
-        time = report_time(Time.now.utc)
-      end
     end
 
     redirect_to report.csv.url
   end
 
-  private
-
-    def report_time(time)
-      time.strftime("%Y%m%d-%H%M")
-    end
 end

ec7b76b7995b287f110da22b957bfb218b5283af
Glenn Roberts <glenn.roberts@siyelo.com>
2011-02-07 12:24:03 +0200

    reannotate, remove used aws-s3 initializer

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,14 +2,18 @@ class Report < ActiveRecord::Base
   has_attached_file :csv, Settings.paperclip.to_options
 end
 
+
 # == Schema Information
 #
 # Table name: reports
 #
-#  id         :integer         not null, primary key
-#  key        :string(255)
-#  csv        :binary
-#  created_at :datetime
-#  updated_at :datetime
+#  id               :integer         not null, primary key
+#  key              :string(255)
+#  created_at       :datetime
+#  updated_at       :datetime
+#  csv_file_name    :string(255)
+#  csv_content_type :string(255)
+#  csv_file_size    :integer
+#  csv_updated_at   :datetime
 #
 
--- config/initializers/amazon_s3.rb
+++
@@ -1,6 +0,0 @@
-if ['staging', 'production'].include? RAILS_ENV
-  AWS::S3::Base.establish_connection!(
-    :access_key_id     => Settings.paperclip.s3_credentials.access_key_id,
-    :secret_access_key => Settings.paperclip.s3_credentials.secret_access_key
-  )
-end

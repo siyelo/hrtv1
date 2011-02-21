45776e74ba0ba17fff604e9fff666ac92ffd9e8f
Glenn Roberts <glenn.roberts@siyelo.com>
2011-02-21 13:31:04 +0200

    recreate schema since both branches had migrations

--- db/fixes/20110221_add_due_date_to_request.rb
+++ db/fixes/20110221_add_due_date_to_request.rb
@@ -1,4 +1,9 @@
 d = DataRequest.find_by_title("FY2010 Workplan and FY2009 Expenditures")
-d.due_date = Date.new(2010, 9, 1)
-d.organization = Organization.find_by_name 'Ministry of Health'
-d.save!
+if d
+  d.due_date = Date.new(2010, 9, 1)
+  d.organization = Organization.find_by_name 'Ministry of Health'
+  d.save!
+  puts "Successfully updated: FY2010 Workplan and FY2009 Expenditures"
+else
+  puts 'WARN - could not find Request: "FY2010 Workplan and FY2009 Expenditures"'
+end

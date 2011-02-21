07c0bf13af96b7edbfb8f8d4fd2a8c51c687c74b
Glenn Roberts <glenn.roberts@siyelo.com>
2011-02-21 13:21:19 +0200

    datafix request

--- db/fixes/20110221_add_due_date_to_request.rb
+++ db/fixes/20110221_add_due_date_to_request.rb
@@ -1,3 +1,4 @@
 d = DataRequest.find_by_title("FY2010 Workplan and FY2009 Expenditures")
 d.due_date = Date.new(2010, 9, 1)
-d.save(false)
+d.organization = Organization.find_by_name 'Ministry of Health'
+d.save!

07ef78e46b24d93026580ba8440343eec42705fc
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-03-01 13:25:08 +0100

    Added budget and spend to data_request factory

--- spec/factories/data_request.rb
+++ spec/factories/data_request.rb
@@ -3,4 +3,6 @@ Factory.define :data_request, :class => DataRequest do |f|
   f.organization  { Factory.create(:organization) }
   f.start_date    { "2010-01-01" }
   f.end_date      { "2012-01-01" }
+  f.budget        { true }
+  f.spend         { true }
 end

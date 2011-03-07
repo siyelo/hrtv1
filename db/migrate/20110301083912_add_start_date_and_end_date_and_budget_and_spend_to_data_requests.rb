981f17b3e6e07415635cc0f038b242ec184f54a6
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-03-07 13:38:52 +0100

    Fixed error with data_requests migration

--- db/migrate/20110301083912_add_start_date_and_end_date_and_budget_and_spend_to_data_requests.rb
+++ db/migrate/20110301083912_add_start_date_and_end_date_and_budget_and_spend_to_data_requests.rb
@@ -2,8 +2,8 @@ class AddStartDateAndEndDateAndBudgetAndSpendToDataRequests < ActiveRecord::Migr
   def self.up
     add_column :data_requests, :start_date, :date
     add_column :data_requests, :end_date, :date
-    add_column :data_requests, :budget, :boolean, :default => 1
-    add_column :data_requests, :spend, :boolean, :default => 1
+    add_column :data_requests, :budget, :boolean, :default => true, :null => false
+    add_column :data_requests, :spend, :boolean, :default => true, :null => false
   end
 
   def self.down

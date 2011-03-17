63afd1ad34f8a60ebcf315ace49b29b3cd58d607
Peter Garbers <peter.garbers@gmail.com>
2011-03-17 12:17:01 +0200

    Seeding Locations with data for kenya

--- db/seed_files/districts.rb
+++ db/seed_files/districts.rb
@@ -3,18 +3,12 @@ puts "\nloading locations (districts)"
 Location.delete_all
 
 case ENV['HRT_COUNTRY']
-when 'rwanda'
-  print "\n Seeding districts for rwanda"
-  FasterCSV.foreach("db/seed_files/districts.csv", :headers=>true) do |row|
-    Location.create!(:short_display => row[0].strip)
-    print "."
-  end
-  load 'db/seed_files/districts_of_rwanda.rb'
 when 'kenya'
   print "\n Seeding districts for kenya"
   FasterCSV.foreach("db/seed_files/kenya/districts.csv", :headers=>true) do |row|
     name = row["short_display"].capitalize
     population = row["population"].to_i
+    Location.create!(:short_display => name)
     District.create!(:name => name, :population => population)
     print "."
   end

dbe66592bff64e28cb7f63e32c99cef3e342bd43
Glenn Roberts <glenn.roberts@siyelo.com>
2011-03-22 15:07:22 +0300

    add swedish kronor and gbp as priority currencies add danish kronor and japanese yen

--- config/initializers/currencies.rb
+++ config/initializers/currencies.rb
@@ -18,5 +18,8 @@ end
 Money::Currency::TABLE[:usd][:priority] = 2
 Money::Currency::TABLE[:eur][:priority] = 3
 Money::Currency::TABLE[:chf][:priority] = 4
-Money::Currency::TABLE[:aud][:priority] = 11
-Money::Currency::TABLE[:gbp][:priority] = 11
+Money::Currency::TABLE[:gbp][:priority] = 5
+Money::Currency::TABLE[:sek][:priority] = 6
+Money::Currency::TABLE[:dkk][:priority] = 7
+Money::Currency::TABLE[:jpy][:priority] = 8
+Money::Currency::TABLE[:aud][:priority] = 11 # otherwise its in the top 10

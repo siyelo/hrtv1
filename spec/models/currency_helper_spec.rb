4349c36796734f67ca482af9c2acbc585d000056
Peter Garbers <peter.garbers@gmail.com>
2011-03-28 14:21:27 +0200

    fixing specs that have to do with currencies

--- spec/models/currency_helper_spec.rb
+++ spec/models/currency_helper_spec.rb
@@ -20,7 +20,11 @@ describe CurrencyHelper do
   
   describe "major currencies" do
     it "should return major currencies" do
-      @foo.major_currencies(Money::Currency::TABLE).should == [:rwf, :usd, :eur, :chf]
+      if ENV['HRT_COUNTRY'].downcase == 'kenya'
+        @foo.major_currencies(Money::Currency::TABLE).should == [:kes, :usd, :eur, :chf]
+      else
+        @foo.major_currencies(Money::Currency::TABLE).should == [:rwf, :usd, :eur, :chf]
+      end
     end
     
     it "should appear in the currency for select twice" do
@@ -32,12 +36,20 @@ describe CurrencyHelper do
   describe "no _to_usd exchange rate" do
     it "should not return a major currency" do
       Money.default_bank.set_rate(:CHF, :USD, nil)
-      @foo.major_currencies(Money::Currency::TABLE).should == [:rwf, :usd, :eur]
+      if ENV['HRT_COUNTRY'].downcase == 'kenya'
+        @foo.major_currencies(Money::Currency::TABLE).should == [:kes, :usd, :eur]
+      else
+        @foo.major_currencies(Money::Currency::TABLE).should == [:rwf, :usd, :eur]
+      end
     end
     
     it "should exclude USD if its not specfied (edge case)" do
       Money.default_bank.set_rate(:USD, :USD, nil)
-      @foo.major_currencies(Money::Currency::TABLE).should == [:rwf, :eur, :chf]
+      if ENV['HRT_COUNTRY'].downcase == 'kenya'
+        @foo.major_currencies(Money::Currency::TABLE).should == [:kes, :eur, :chf]
+      else
+        @foo.major_currencies(Money::Currency::TABLE).should == [:rwf, :eur, :chf]
+      end
     end
 
     it "should not return a currency for select" do

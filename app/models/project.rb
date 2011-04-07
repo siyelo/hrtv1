2489321937972f9a70eb171edc59733dcbe59544
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-04-07 13:39:23 +0200

    Fixes in UFS for nil objects

--- app/models/project.rb
+++ app/models/project.rb
@@ -160,7 +160,8 @@ class Project < ActiveRecord::Base
   end
 
   def ultimate_funding_sources
-    trace_ultimate_funding_source(funding_flows.map(&:from))
+    funders = funding_flows.map(&:from).reject{|f| f.nil?}
+    trace_ultimate_funding_source(funders)
   end
 
   private
@@ -173,8 +174,8 @@ class Project < ActiveRecord::Base
       funding_sources = []
 
       organizations.each do |organization|
-        in_flows_amount  = organization.in_flows.map(&:budget).sum
-        out_flows_amount = organization.out_flows.map(&:budget).sum
+        in_flows_amount  = organization.in_flows.map{|ff| ff.budget || 0}.sum
+        out_flows_amount = organization.out_flows.map{|ff| ff.budget || 0}.sum
 
         # if more out flows than in flows it's a funding source
         if out_flows_amount > in_flows_amount
@@ -183,7 +184,8 @@ class Project < ActiveRecord::Base
 
         if organization.in_flows.present? && !traced.include?(organization)
           traced << organization
-          funding_sources.concat(trace_ultimate_funding_source(organization.in_flows.map(&:from), traced))
+          funders = organization.in_flows.map(&:from).reject{|f| f.nil?}
+          funding_sources.concat(trace_ultimate_funding_source(funders, traced))
         end
       end
 
---
+++ db/reports/ultimate_funding_sources.rb
@@ -0,0 +1,26 @@
+require 'fastercsv'
+
+projects = Project.find(:all, :limit => 5)
+total = projects.length
+
+csv = FasterCSV.generate do |csv|
+  # header
+  row = ['Project ID', 'Project name', 'Ultimate funding sources']
+  csv << row
+
+  # data
+  projects.each_with_index do |project, index|
+    puts "Checking UFS for project with id: #{project.id} | #{index + 1}/#{total}"
+
+    row = []
+    row << project.id
+    row << project.name
+    row << project.ultimate_funding_sources.map(&:name).join(', ')
+    csv << row
+  end
+end
+
+File.open(File.join(Rails.root, 'db', 'reports', 'ultimate_funding_sources.csv'), 'w') do |file|
+  file.puts csv
+end
+

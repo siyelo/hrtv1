42f59d0bfd37d3676e606c5bb2686e11edfbd354
Greg Nelson <gln@georgetown.edu>
2011-04-13 16:07:19 +0200

    fix org types

--- db/migrate/20110413124104_update_organization_types.rb
+++ db/migrate/20110413124104_update_organization_types.rb
@@ -1,8 +1,9 @@
 class UpdateOrganizationTypes < ActiveRecord::Migration
   def self.up
     FasterCSV.foreach("db/fixes/update_org_types.csv", :headers=>true) do |row|
-      o = Organization.find_by_name(row[0])
-      o.update_attributes(:raw_type => row[1])
+      puts row[1]
+      o = row[0].blank? ? Organization.find_by_name(row[1]) : Organization.find(row[0])
+      o.update_attributes(:raw_type => row[2])
     end
   end
    

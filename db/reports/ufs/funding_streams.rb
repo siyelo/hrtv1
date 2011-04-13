95bd9f147c5c4b8cec71437515b90f061b639f5f
Greg Nelson <gln@georgetown.edu>
2011-04-13 14:37:27 +0200

    update file read in script

--- db/reports/ufs/funding_streams.rb
+++ db/reports/ufs/funding_streams.rb
@@ -3,9 +3,10 @@ puts "Loading funding streams"
 FundingStream.delete_all
 
 FasterCSV.foreach("db/reports/ufs/funding_streams.csv", :headers=>true) do |row|
-  FundingStream.create!(:project_id => row[0], 
-                        :organization_ufs_id => row[1],
-                        :organization_fa_id => row[2])
-  print "."
-  puts
+  ufs = Organization.find_by_name(row[1])
+  fa = Organization.find_by_name(row[2])
+  f=FundingStream.create!(:project_id => row[0], 
+                        :organization_ufs_id => ufs.id,
+                        :organization_fa_id => fa.id)
+  puts "#{ufs} - #{fa}"
 end

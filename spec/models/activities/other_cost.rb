29d895dc0bf5dfe3189e93af5452b7d0e358db8b
Peter Garbers <peter.garbers@gmail.com>
2011-04-18 18:52:46 +0200

    added fields to othercosts to make it more similar to activities with basic specs
    removed valiations that aren't needed

--- spec/models/activities/other_cost.rb
+++ spec/models/activities/other_cost.rb
@@ -1,6 +1,52 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe OtherCost do
+  describe "associations" do
+    it { should belong_to :provider }
+    it { should belong_to :data_response }
+    it { should belong_to :project }
+    it { should have_and_belong_to_many :locations }
+    it { should have_and_belong_to_many :organizations }
+    it { should have_and_belong_to_many :beneficiaries }
+    it { should have_many :sub_implementers }
+    it { should have_many :codes }
+    it { should have_many :code_assignments }
+    it { should have_many :coding_budget }
+    it { should have_many :coding_budget_cost_categorization }
+    it { should have_many :coding_budget_district }
+    it { should have_many :coding_spend }
+    it { should have_many :coding_spend_cost_categorization }
+    it { should have_many :coding_spend_district }
+  end
+
+  describe "attributes" do
+    it { should allow_mass_assignment_of(:name) }
+    it { should allow_mass_assignment_of(:description) }
+    it { should allow_mass_assignment_of(:start_date) }
+    it { should allow_mass_assignment_of(:end_date) }
+    it { should allow_mass_assignment_of(:project_id) }
+    it { should allow_mass_assignment_of(:budget) }
+    it { should allow_mass_assignment_of(:spend) }
+    it { should allow_mass_assignment_of(:budget_q4_prev) }
+    it { should allow_mass_assignment_of(:budget_q1) }
+    it { should allow_mass_assignment_of(:budget_q2) }
+    it { should allow_mass_assignment_of(:budget_q3) }
+    it { should allow_mass_assignment_of(:budget_q4) }
+    it { should allow_mass_assignment_of(:spend_q4_prev) }
+    it { should allow_mass_assignment_of(:spend_q1) }
+    it { should allow_mass_assignment_of(:spend_q2) }
+    it { should allow_mass_assignment_of(:spend_q3) }
+    it { should allow_mass_assignment_of(:spend_q4) }
+    it { should allow_mass_assignment_of(:location_ids) }
+    it { should allow_mass_assignment_of(:beneficiary_ids) }
+    it { should allow_mass_assignment_of(:provider_id) }
+    it { should allow_mass_assignment_of(:text_for_provider) }
+    it { should allow_mass_assignment_of(:text_for_beneficiaries) }
+    it { should allow_mass_assignment_of(:text_for_targets) }
+    it { should allow_mass_assignment_of(:approved) }
+    it { should allow_mass_assignment_of(:organization_ids) }
+  end
+  
 
   describe "classified?" do
     before :each do

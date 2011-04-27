282297dbb35a2a6c4ebe2c35b95e28f8433caf22
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-04-27 10:57:35 +0200

    Fixed bug in funder_projects_select on bulk edit page
      * when from org is nil
    
    PT: https://www.pivotaltracker.com/story/show/12729913

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,8 +1,16 @@
 module ProjectsHelper
+
   def funder_projects_select(from_org, project_id)
-    flows = []
-    funder_projects = from_org.projects.map{|op| [op.name, op.id] if op.id != project_id}.compact
-    [["Please select a project...",""]] +
-      funder_projects + [["<Project not listed or unknown>", 0]]
+    if from_org
+      funder_projects = from_org.projects.select{|op| [op.name, op.id] if op.id != project_id}
+    else
+      funder_projects = []
+    end
+
+    options = [["Please select a project...", ""]]
+    options.concat(funder_projects)
+    options << ["<Project not listed or unknown>", 0]
+
+    options
   end
 end

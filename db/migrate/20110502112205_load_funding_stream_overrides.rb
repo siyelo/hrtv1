992cb53692a4f497095016799982a0f7e397a346
Greg Nelson <gln@georgetown.edu>
2011-05-02 13:29:31 +0200

    migration runs in staging

--- db/migrate/20110502112205_load_funding_stream_overrides.rb
+++ db/migrate/20110502112205_load_funding_stream_overrides.rb
@@ -1,6 +1,6 @@
 class LoadFundingStreamOverrides < ActiveRecord::Migration
   def self.up
-    load 'db/reports/ufs/funding_streams.rb' if ["production", "development"].include?(RAILS_ENV)
+    load 'db/reports/ufs/funding_streams.rb' if ["production", "staging", "development"].include?(RAILS_ENV)
   end
 
   def self.down

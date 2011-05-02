ec752491fab3f89f18cdcaad4c943202c669d799
Greg Nelson <gln@georgetown.edu>
2011-05-02 13:26:26 +0200

    friendly migration to load overrided funding streams

---
+++ db/migrate/20110502112205_load_funding_stream_overrides.rb
@@ -0,0 +1,9 @@
+class LoadFundingStreamOverrides < ActiveRecord::Migration
+  def self.up
+    load 'db/reports/ufs/funding_streams.rb' if ["production", "development"].include?(RAILS_ENV)
+  end
+
+  def self.down
+    puts 'already seeded funding streams, irreversible migration'
+  end
+end

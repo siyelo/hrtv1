7a796926014976f873581d3b4398ddc5bc498c59
Glenn Roberts <glenn.roberts@siyelo.com>
2011-05-09 11:44:13 +0200

    update hudson to jenkins

--- lib/tasks/ci.sh
+++ lib/tasks/ci.sh
@@ -1,16 +1,16 @@
 # A basic setup for a CI server (Hudson)
 #
 # You may need to do this first per CI env
-#   rvm gemset create ubuzima-glennr
-#   rvm gemset import ubuzima-glennr
+#   rvm gemset create hrt
+#   rvm gemset import hrt
 
 #1
 
 #!/bin/bash
-source /var/lib/hudson/.rvm/scripts/rvm
+source /var/lib/jenkins/.rvm/scripts/rvm
 rvm use ree-1.8.7-2010.02
-rvm gemset use ubuzima-glennr
-rvm gemset import ubuzima-glennr
+rvm gemset use hrt
+rvm gemset import hrt
 
 #2
 cp $WORKSPACE/config/database.yml.sample $WORKSPACE/config/database.yml
@@ -18,29 +18,29 @@ cp $WORKSPACE/config/settings.secret.example.yml $WORKSPACE/config/settings.secr
 
 #3
 #!/bin/bash
-source /var/lib/hudson/.rvm/scripts/rvm
-rvm use ree-1.8.7-2010.02@ubuzima-glennr
+source /var/lib/jenkins/.rvm/scripts/rvm
+rvm use ree-1.8.7-2010.02@hrt
 export RAILS_ENV=test
 rake setup_quick --trace
 
 #4
 #!/bin/bash
-source /var/lib/hudson/.rvm/scripts/rvm
-rvm use ree-1.8.7-2010.02@ubuzima-glennr
+source /var/lib/jenkins/.rvm/scripts/rvm
+rvm use ree-1.8.7-2010.02@hrt
 export RAILS_ENV=test
 rake test:units
 
 #5
 #!/bin/bash
-source /var/lib/hudson/.rvm/scripts/rvm
-rvm use ree-1.8.7-2010.02@ubuzima-glennr
+source /var/lib/jenkins/.rvm/scripts/rvm
+rvm use ree-1.8.7-2010.02@hrt
 export RAILS_ENV=test
 spec spec
 
 #6
 #!/bin/bash
-source /var/lib/hudson/.rvm/scripts/rvm
-rvm use ree-1.8.7-2010.02@ubuzima-glennr
+source /var/lib/jenkins/.rvm/scripts/rvm
+rvm use ree-1.8.7-2010.02@hrt
 export RAILS_ENV=cucumber
 # http://blog.kabisa.nl/2010/05/24/headless-cucumbers-and-capybaras-with-selenium-and-hudson/
 # and http://markgandolfo.com/2010/07/01/hudson-ci-server-running-cucumber-in-headless-mode-xvfb

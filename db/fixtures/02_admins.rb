16779ae0c45fd5d4bc22f66d469c7770b0642b8b
Glenn Roberts <glenn.roberts@siyelo.com>
2011-05-16 20:31:35 +0200

    fix seeding, tidy views

--- db/fixtures/02_admins.rb
+++ db/fixtures/02_admins.rb
@@ -1,5 +1,13 @@
 require 'factory_girl'
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'../','../','spec','factories','**','*.rb'))].each {|f| require f}
 
-admin = Factory(:admin)
-print "=> admin user: #{admin.name} created (org: #{admin.organization.name})"
+begin
+  puts "creating admin"
+  org = Factory(:organization, :name => 'internal_admin_org')
+  admin = Factory(:admin, :username => 'admin', :email => 'admin@eg.com', :organization => org)
+rescue ActiveRecord::RecordInvalid => e
+  puts e.message
+  puts "   Do you already have an org 'admin_org' or user named 'admin'? "
+else
+  puts "=> admin user: #{admin.name} created (org: #{admin.organization.name})"
+end

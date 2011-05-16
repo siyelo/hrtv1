731baeea3c39bc47b68e86f34ba0835290904330
Glenn Roberts <glenn.roberts@siyelo.com>
2011-05-16 19:07:17 +0200

    generate dev data using factories now

--- db/fixtures/03_dummy_reporters.rb
+++ db/fixtures/03_dummy_reporters.rb
@@ -1,41 +1,10 @@
-org = Organization.find_or_create_by_name("internal_for_dev2")
-saved = User.find_or_create_by_username('reporter',
-              :email => 'reporter@siyelo.com',
-              :password => 'password',
-              :password_confirmation => 'password',
-              :organization_id => org.id,
-              :roles => ['reporter'])
+require 'factory_girl'
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'../','../','spec','factories','**','*.rb'))].each {|f| require f}
+response = Factory.create(:data_response, :organization => reporter.organization)
+Factory(:project, :data_response => response, :budget => 100, :spend => 80)
+Factory(:activity_fully_coded, :data_response => response, :project => @project)
+Factory(:other_cost_fully_coded, :data_response => response, :project => @project)
+print " added sample data for reporter #{reporter.name}"
 
-print "  WARN: reporter not created" unless saved
-
-org = Organization.find_or_create_by_name("internal_for_dev3")
-saved = User.find_or_create_by_username('reporter2',
-              :email => 'reporter2@siyelo.com',
-              :password => 'password',
-              :password_confirmation => 'password',
-              :organization_id => org.id,
-              :roles => ['reporter'])
-
-print "  WARN: reporter2 not created" unless saved
-
-org =  Organization.find_or_create_by_name("internal_for_dev3")
-user = User.find_or_create_by_username('activity_manager',
-              :email => 'activity_manager@siyelo.com',
-              :password => 'password',
-              :password_confirmation => 'password',
-              :organization_id => org.id,
-              :roles => ['activity_manager'])
-dr = org.data_responses.first
-user.current_data_response = dr
-user.save!
-print "  WARN: activity_manager not created" unless user
-
-org = Organization.find_or_create_by_name("internal_for_dev4")
-saved = User.find_or_create_by_username('developer',
-              :email => 'support@siyelo.com',
-              :password => 'password',
-              :password_confirmation => 'password',
-              :organization_id => org.id,
-              :roles => ['reporter'])
-
-print "  WARN: developer account not created" unless saved
+am = Factory(:activity_manager)
+print "=> activity_manager #{am.name} created (org: #{am.organization.name})"

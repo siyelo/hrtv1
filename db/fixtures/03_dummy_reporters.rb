1902df51e533bac05c77789dddeb6a07d28e48ed
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-05-16 11:55:30 +0200

    Fixed dummy reporters fixture

--- db/fixtures/03_dummy_reporters.rb
+++ db/fixtures/03_dummy_reporters.rb
@@ -1,17 +1,19 @@
+org = Organization.find_or_create_by_name("internal_for_dev2")
 saved = User.find_or_create_by_username('reporter',
               :email => 'reporter@siyelo.com',
               :password => 'password',
               :password_confirmation => 'password',
-              :organization => Organization.find_or_create_by_name("internal_for_dev2"),
+              :organization_id => org.id,
               :roles => ['reporter'])
 
 print "  WARN: reporter not created" unless saved
 
+org = Organization.find_or_create_by_name("internal_for_dev3")
 saved = User.find_or_create_by_username('reporter2',
               :email => 'reporter2@siyelo.com',
               :password => 'password',
               :password_confirmation => 'password',
-              :organization => Organization.find_or_create_by_name("internal_for_dev3"),
+              :organization_id => org.id,
               :roles => ['reporter'])
 
 print "  WARN: reporter2 not created" unless saved
@@ -21,18 +23,19 @@ user = User.find_or_create_by_username('activity_manager',
               :email => 'activity_manager@siyelo.com',
               :password => 'password',
               :password_confirmation => 'password',
-              :organization => org,
+              :organization_id => org.id,
               :roles => ['activity_manager'])
 dr = org.data_responses.first
 user.current_data_response = dr
 user.save!
 print "  WARN: activity_manager not created" unless user
 
+org = Organization.find_or_create_by_name("internal_for_dev4")
 saved = User.find_or_create_by_username('developer',
               :email => 'support@siyelo.com',
               :password => 'password',
               :password_confirmation => 'password',
-              :organization => Organization.find_or_create_by_name("internal_for_dev4"),
+              :organization_id => org.id,
               :roles => ['reporter'])
 
 print "  WARN: developer account not created" unless saved

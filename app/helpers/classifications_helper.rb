ac85f899783bd901cfbe6a0c7ddd3e5b71e801d6
Glenn Roberts <glenn.roberts@siyelo.com>
2011-05-17 18:47:23 +0200

    fix bug when no params entered

--- app/helpers/classifications_helper.rb
+++ app/helpers/classifications_helper.rb
@@ -1,17 +1,17 @@
 module ClassificationsHelper
-  def spend_coding_type
-    if get_coding_type(params[:coding_type]) == :spend
-      params[:coding_type]
+  def spend_coding_type(type)
+    if get_coding_type(type) == :spend
+      type
     else
-      Activity::CLASSIFICATION_MAPPINGS[params[:coding_type]]
+      Activity::CLASSIFICATION_MAPPINGS[type]
     end
   end
 
-  def budget_coding_type
-    if get_coding_type(params[:coding_type]) == :budget
-      params[:coding_type]
+  def budget_coding_type(type)
+    if get_coding_type(type) == :budget
+      type
     else
-      Activity::CLASSIFICATION_MAPPINGS.invert[params[:coding_type]]
+      Activity::CLASSIFICATION_MAPPINGS.invert[type]
     end
   end
 end

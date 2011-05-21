d66acbb883edd603506267383833d240be924bc2
Glenn Roberts <glenn.roberts@siyelo.com>
2011-05-21 16:35:01 +0200

    add extra span to currency helper to allow targeting of just the amount

--- lib/number_helper.rb
+++ lib/number_helper.rb
@@ -2,37 +2,37 @@ module NumberHelper
   include ActionView::Helpers::NumberHelper
 
   def n2c(value, unit = "")
-    number_to_currency(value, 
-                      :separator => ".", 
-                      :unit => unit, 
+    number_to_currency(value,
+                      :separator => ".",
+                      :unit => unit,
                       :delimiter => ",")
   end
-  
+
   def n2cs(value, unit = "")
-    number_to_currency(value, 
-                      :separator => ".", 
-                      :unit => "<span class=\"currency\">#{unit}</span>", 
-                      :delimiter => ",", 
+    number_to_currency(value,
+                      :separator => ".",
+                      :unit => "<span class=\"currency\">#{unit}</span>",
+                      :delimiter => ",",
                       :format => "%u %u")
   end
-  
+
   def n2crs(value, unit = "")
-    number_to_currency(value, 
-                      :separator => ".", 
-                      :unit => "<span class=\"currency\">#{unit}</span>", 
-                      :delimiter => ",", 
+    number_to_currency(value,
+                      :separator => ".",
+                      :unit => "<span class=\"currency\">#{unit}</span>",
+                      :delimiter => ",",
                       :format => "%n %u")
   end
-  
+
   def n2cr(value, unit = "")
-    number_to_currency(value, 
-                      :separator => ".", 
-                      :unit => unit, 
-                      :delimiter => ",", 
+    number_to_currency(value,
+                      :separator => ".",
+                      :unit => unit,
+                      :delimiter => ",",
                       :format => "%n %u")
   end
-  
-  
+
+
   def n2cnd(value, unit = "")
     number_to_currency(value,
                        :separator => ".",
@@ -50,21 +50,21 @@ module NumberHelper
                        :format => "%n %u",
                        :precision => 0)
   end
-  
+
   def n2cndrs(value, unit = "")
-    number_to_currency(value, 
-                      :separator => ".", 
-                      :unit => "<span class=\"currency\">#{unit}</span>", 
-                      :delimiter => ",", 
-                      :format => "%n %u",
+    number_to_currency(value,
+                      :separator => ".",
+                      :unit => "#{unit}",
+                      :delimiter => ",",
+                      :format => "<span class=\"amount\">%n</span> <span class=\"currency\">%u</span>",
                       :precision => 0)
   end
-  
+
   def n2cnds(value, unit = "")
-    number_to_currency(value, 
-                      :separator => ".", 
-                      :unit => "<span class=\"currency\">#{unit}</span>", 
-                      :delimiter => ",", 
+    number_to_currency(value,
+                      :separator => ".",
+                      :unit => "<span class=\"currency\">#{unit}</span>",
+                      :delimiter => ",",
                       :format => "%u %n",
                       :precision => 0)
   end
@@ -81,7 +81,7 @@ module NumberHelper
       to_usd && from_usd ? to_usd * from_usd : 1
     end
   end
-  
+
   def universal_currency_converter(amount, from, to)
     amount = 0 if amount.blank?
     amount * currency_rate(from, to)

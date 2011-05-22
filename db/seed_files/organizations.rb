1d18007b8a3fdb1c2fa7c2ca9e5b064b051a10f1
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-05-22 20:11:27 +0200

    Added kenya organizations seed

--- db/seed_files/organizations.rb
+++ db/seed_files/organizations.rb
@@ -1,12 +1,14 @@
-Organization.delete_all
+#Organization.delete_all
 
 case ENV['HRT_COUNTRY']
 when 'kenya'
   print "\n Seeding organizations for kenya"
-  FasterCSV.foreach("db/seed_files/kenya/organization.csv", :headers=>true) do |row|
-    name = row["name"]
-    raw_type = row["raw_type"]
-    Organization.create!(:name => name, :raw_type => raw_type)
+  FasterCSV.foreach("db/seed_files/kenya/organizations.csv", :headers=>true) do |row|
+    organization = Organization.find_or_initialize_by_name(row["name"])
+    organization.raw_type = row["raw_type"]
+    organization.old_type = row["old_type"]
+    organization.acronym  = row["acronym"]
+    organization.save
     print "."
   end
   print "\n Finished Seeding organizations for kenya"

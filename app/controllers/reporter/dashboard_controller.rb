dbc8ed1e610dc3dd23ca7b3d72d1c752bf8466ee
Glenn Roberts <glenn.roberts@siyelo.com>
2011-05-25 02:39:54 +0300

    fix dashboard where no response existed before

--- app/controllers/reporter/dashboard_controller.rb
+++ app/controllers/reporter/dashboard_controller.rb
@@ -5,10 +5,10 @@ class Reporter::DashboardController < Reporter::BaseController
     @responses      = current_user.data_responses
     @comments       = Comment.on_all(current_user.organization).limit(5)
     # hack to show something dynamic on dashboard
-    @projects      = current_user.organization.projects
-    @response = @projects.first.response
+    @projects       = current_user.organization.projects
+    @response       = current_user.organization.data_responses.first || DataResponse.new
     # /hack
   end
-  
+
 end
 

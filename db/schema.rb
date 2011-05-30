147acecc59d6ffe8ac77da51865a43c2b6a6e0f3
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-05-30 14:02:01 +0200

    Added migration for removing hanging code assignments
    PT: https://www.pivotaltracker.com/story/show/5312482

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110519112419) do
+ActiveRecord::Schema.define(:version => 20110530114145) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"

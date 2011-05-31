b00f76e725d72a0371a1e47dfc25bd5b9ec3827a
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-05-31 10:58:52 +0200

    Clean sessions table
    PT: https://www.pivotaltracker.com/story/show/9581695

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110530114145) do
+ActiveRecord::Schema.define(:version => 20110531083206) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"

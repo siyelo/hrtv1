91fc62b0da82e8b08c31601e711c666419978e05
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-06-02 12:26:50 +0200

    More migration fixes

--- db/migrate/20110517171726_setting_default_values_on_data_request.rb
+++ db/migrate/20110517171726_setting_default_values_on_data_request.rb
@@ -1,5 +1,6 @@
 class SettingDefaultValuesOnDataRequest < ActiveRecord::Migration
   def self.up
+    DataRequest.reset_column_information
     load 'db/fixes/add_default_values_to_datarequests.rb'
   end
 

c0a2b141c8897e173fad04df4c8fc1a95b6ee146
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-06-02 15:32:07 +0200

    Fiscal year start end date
    PT: https://www.pivotaltracker.com/story/show/13600519

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,7 +1,7 @@
 module ActivitiesHelper
   def get_funding_sources(projects)
     funding_sources = {}
-    projects.each do |project| 
+    projects.each do |project|
       funding_sources[project.id] = funding_sources_options(project.in_flows)
     end
     funding_sources.to_json

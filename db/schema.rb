60de042fec1866929e6abfb6c6793ea656c37d7c
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-06-17 14:31:58 +0200

    [Fixes #14454785] Added Central Level location
    PT: https://www.pivotaltracker.com/story/show/14454785

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110616111746) do
+ActiveRecord::Schema.define(:version => 20110617113316) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"

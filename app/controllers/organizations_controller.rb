66ae2f5e8fb9dd13b35c8578117b7e4d1f81e7e4
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-06-21 10:43:43 +0200

    Removed unused code for creting new org

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,14 +1,5 @@
 class OrganizationsController < Reporter::BaseController
 
-  def create
-    organization = Organization.new
-    organization.name = params[:name]
-    organization.save
-    respond_to do |format|
-      format.js {render :json => organization.to_json}
-    end
-  end
-
   def edit
     @organization = current_user.organization
     current_user.current_data_response = @organization.data_responses.first
@@ -25,6 +16,5 @@ class OrganizationsController < Reporter::BaseController
       render :action => :edit
     end
   end
-
 end
 

7e0c38471cd4c8e96fae6dac1c0addc6bada5e20
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-06-22 12:06:33 +0200

    Switch current data response when editing it

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,8 +2,9 @@ class OrganizationsController < Reporter::BaseController
 
   def edit
     @organization = current_user.organization
-    @organization.valid?
-    current_user.current_data_response = @organization.data_responses.first
+    @organization.valid? # trigger validation errors
+    data_response = params[:data_response_id].present? ? @organization.data_responses.find(params[:data_response_id]) : @organization.data_responses.first
+    current_user.current_data_response = data_response
     current_user.save
   end
 

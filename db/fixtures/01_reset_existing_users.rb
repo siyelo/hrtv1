8b3bcf8054693462492c2eab42947b4440327eec
Glenn Roberts <glenn.roberts@siyelo.com>
2011-06-23 10:41:53 +0200

    db:populate wont reset existing passwords if accidentally run in production

--- db/fixtures/01_reset_existing_users.rb
+++ db/fixtures/01_reset_existing_users.rb
@@ -1,5 +1,10 @@
+
 puts "resetting all existing users passwords"
 
-User.all.each{|u| u.password = 'si@yelo'; u.password_confirmation = 'si@yelo'; u.save}
+unless RAILS_ENV == 'production'
+  User.all.each{|u| u.password = 'si@yelo'; u.password_confirmation = 'si@yelo'; u.save}
+else
+  puts "refusing to run in production!"
+end
 
 puts "=> Done"

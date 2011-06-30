d61156450fd882ca415fa17dbb60f3bcd4e7f287
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-06-30 13:37:47 +0200

    Refactored reporter/activity manager dashboards
    * Refactored reporter/activity manager dashboards
    * Removed hacked URLs
    * Fixed comments for activity manager

--- app/controllers/reporter/dashboard_controller.rb
+++ app/controllers/reporter/dashboard_controller.rb
@@ -1,12 +1,9 @@
 class Reporter::DashboardController < Reporter::BaseController
   def index
-    @responses     = current_user.organization.data_responses.ordered.all
-    @comments      = Comment.on_all(@responses.map{|r| r.id}).limit(5)
-    @user          = current_user
-    @request       = current_user.current_request
-    @organizations = current_user.organizations
-    organization_ids = @organizations.map{|o| o.id}
     if current_user.activity_manager?
+      @responses     = current_user.organization.data_responses.ordered.all
+      @organizations = current_user.organizations
+      organization_ids = @organizations.map{|o| o.id}
       @approved_orgs = Activity.with_organization.count(:all,
         :conditions =>  ["organization_id in (?) AND
                           data_responses.data_request_id = ? AND
@@ -19,7 +16,11 @@ class Reporter::DashboardController < Reporter::BaseController
                          submitted_at IS NOT NULL", organization_ids],
         :order => 'submitted_at DESC', :limit => 3)
       dr_ids = current_user.organizations.map{|o| o.data_responses.map{|dr| dr.id }}.flatten
+      dr_ids += current_user.organization.data_responses.map{|dr| dr.id }
       @comments  = Comment.on_all(dr_ids).limit(5)
+    else
+      @responses     = current_user.organization.data_responses.ordered.all
+      @comments      = Comment.on_all(@responses.map{|r| r.id}).limit(5)
     end
   end
 end

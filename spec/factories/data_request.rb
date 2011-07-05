06251009d30642b81e173bf207f2345aefe98b7d
Peter Garbers <peter.garbers@gmail.com>
2011-07-05 11:11:35 +0200

    removed dataresponse spend/budget present checks

--- spec/factories/data_request.rb
+++ spec/factories/data_request.rb
@@ -5,7 +5,5 @@ Factory.define :data_request, :class => DataRequest do |f|
   f.end_date          { "2012-01-01" }
   f.due_date          { "2012-09-01" }
   f.service_levels    { true }
-  f.budget            { true }
-  f.spend             { true }
   f.budget_by_quarter { false }
 end

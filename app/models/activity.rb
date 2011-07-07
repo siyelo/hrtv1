4c96bcf882ce342d19a964b070e376f9d5a714d0
Naum Kostovski <kostovski.naum@gmail.com>
2011-07-07 14:56:11 +0200

    fixed correct quarter methods on the response object

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -192,15 +192,15 @@ class Activity < ActiveRecord::Base
       (100 - header_row.length).times{ header_row << nil}
       header_row << 'Id'
       
-      header_row[5] = "#{response.quarters_months('q1')} Spend"
-      header_row[6] = "#{response.quarters_months('q2')} Spend"
-      header_row[7] = "#{response.quarters_months('q3')} Spend"
-      header_row[8] = "#{response.quarters_months('q4')} Spend"
+      header_row[5] = "#{response.spend_quarters_months('q1')} Spend"
+      header_row[6] = "#{response.spend_quarters_months('q2')} Spend"
+      header_row[7] = "#{response.spend_quarters_months('q3')} Spend"
+      header_row[8] = "#{response.spend_quarters_months('q4')} Spend"
       
-      header_row[10] = "#{response.quarters_months('q1')} Budget"
-      header_row[11] = "#{response.quarters_months('q2')} Budget"
-      header_row[12] = "#{response.quarters_months('q3')} Budget"
-      header_row[13] = "#{response.quarters_months('q4')} Budget"
+      header_row[10] = "#{response.budget_quarters_months('q1')} Budget"
+      header_row[11] = "#{response.budget_quarters_months('q2')} Budget"
+      header_row[12] = "#{response.budget_quarters_months('q3')} Budget"
+      header_row[13] = "#{response.budget_quarters_months('q4')} Budget"
       
       csv << header_row
 

6d101a22c86f9aa865a114fd89746d8028b598af
Glenn Roberts <glenn.roberts@siyelo.com>
2011-07-12 17:20:08 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(:version => 20110711154419) do
     t.decimal  "cached_amount_in_usd", :default => 0.0
   end
 
-  add_index "code_assignments", ["activity_id", "code_id", "type"], :name => "index_code_assignments_on_activity_id_and_code_id_and_type"
   add_index "code_assignments", ["code_id"], :name => "index_code_assignments_on_code_id"
 
   create_table "codes", :force => true do |t|

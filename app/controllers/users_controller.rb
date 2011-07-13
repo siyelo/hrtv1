074d3dfe74f04f0121bf18d63a7e32d62fdb33d1
Naum Kostovski <kostovski.naum@gmail.com>
2011-07-13 16:40:56 +0200

    Fixed redirect issue to redirect user to dashboard if no HTTP_REFERER is set
    PT: https://www.pivotaltracker.com/story/show/15705339

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,21 @@
 class UsersController < ApplicationController
   # set the user's 'current response' based on the given Request id
-  def set_request
+  def set_request 
     change_user_current_response(DataRequest.find(params[:id]))
-    redirect_to :back
+    redirect_back
   end
 
   # set the user's 'current response' to the one associated with the latest Request
-  def set_latest_request
+  def set_latest_request 
     current_user.set_current_response_to_latest!
-    flash[:notice] = latest_request_message(current_user.current_response.request)
+    flash[:notice] = latest_request_message(current_user.current_response.request) 
+    redirect_back
+  end
+
+  private
+  def redirect_back
     redirect_to :back
+  rescue ActionController::RedirectBackError
+    redirect_to dashboard_path
   end
 end

529da7df7581658fefa6a802c579765acffab7c2
Peter Garbers <peter.garbers@gmail.com>
2011-07-19 17:37:12 +0200

    annotated currencies

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,14 @@
 class Currency < ActiveRecord::Base
 end
+
+# == Schema Information
+#
+# Table name: currencies
+#
+#  id         :integer         not null, primary key
+#  conversion :string(255)
+#  rate       :float
+#  created_at :datetime
+#  updated_at :datetime
+#
+

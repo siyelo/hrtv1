093bcec5eaf04ccf3d8074cb4f9a7cc6a82a6853
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-07-20 10:34:20 +0200

    Fixed migrations to run on empty db

--- db/migrate/20110608080156_fix_currencies.rb
+++
@@ -1,9 +0,0 @@
-class FixCurrencies < ActiveRecord::Migration
-  def self.up
-    load 'db/fixes/20101121_clean_currencies.rb'
-  end
-
-  def self.down
-    puts 'ireversible migration'
-  end
-end

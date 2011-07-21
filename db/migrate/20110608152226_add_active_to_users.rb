26eadacd30755755888526016a9e80d3d4ebdc15
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-07-21 14:52:37 +0200

    Fixed migraition for seting defaults for active field in user model

--- db/migrate/20110608152226_add_active_to_users.rb
+++ db/migrate/20110608152226_add_active_to_users.rb
@@ -1,6 +1,8 @@
 class AddActiveToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :active, :boolean, :default => false
+
+    User.reset_column_information
     load 'db/fixes/add_active_to_present_users.rb'
   end
 

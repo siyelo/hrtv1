487b34fd5f596dae60bbcf52cbee34dc343267c3
Dalibor Nasevic and Peter Garbers <dev+dalibor+peter@siyelo.com>
2011-07-21 14:47:58 +0200

    Fixed user activation in migrations

--- db/schema.rb
+++ db/schema.rb
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(:version => 20110721120922) do
     t.decimal  "cached_amount_in_usd", :default => 0.0
   end
 
+  add_index "code_assignments", ["activity_id", "code_id", "type"], :name => "index_code_assignments_on_activity_id_and_code_id_and_type"
   add_index "code_assignments", ["code_id"], :name => "index_code_assignments_on_code_id"
 
   create_table "codes", :force => true do |t|
@@ -126,6 +127,13 @@ ActiveRecord::Schema.define(:version => 20110721120922) do
   add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
   add_index "comments", ["commentable_type"], :name => "index_comments_on_commentable_type"
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "currencies", :force => true do |t|
+    t.string   "conversion"
+    t.float    "rate"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "data_requests", :force => true do |t|
     t.integer  "organization_id"
@@ -173,6 +181,15 @@ ActiveRecord::Schema.define(:version => 20110721120922) do
     t.string   "name"
     t.integer  "population"
     t.integer  "old_location_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "field_helps", :force => true do |t|
+    t.string   "attribute_name"
+    t.string   "short"
+    t.text     "long"
+    t.integer  "model_help_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -241,6 +258,15 @@ ActiveRecord::Schema.define(:version => 20110721120922) do
   create_table "locations_projects", :id => false, :force => true do |t|
     t.integer "location_id"
     t.integer "project_id"
+  end
+
+  create_table "model_helps", :force => true do |t|
+    t.string   "model_name"
+    t.string   "short"
+    t.text     "long"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "comments_count", :default => 0
   end
 
   create_table "organizations", :force => true do |t|
@@ -351,8 +377,5 @@ ActiveRecord::Schema.define(:version => 20110721120922) do
     t.string   "invite_token"
     t.boolean  "active",                   :default => false
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email"
-  add_index "users", ["perishable_token"], :name => "index_users_on_perishable_token"
 
 end

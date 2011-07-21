b4b2854dd973840fbeb260d576993d4f9d368564
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-07-21 15:30:37 +0200

    Added host for cucumber and test environments

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -19,4 +19,5 @@ config.action_controller.allow_forgery_protection    = false
 # Tell Action Mailer not to deliver emails to the real world.
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
-config.action_mailer.delivery_method = :test
+config.action_mailer.delivery_method                 = :test
+config.action_mailer.default_url_options             = { :host => 'localhost:3000' }

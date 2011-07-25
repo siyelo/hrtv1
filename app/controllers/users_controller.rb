8cc6f7a3bac9ec5ee6c1cfbb74e86ec4be416e7c
Naum Kostovski <kostovski.naum@gmail.com>
2011-07-25 14:31:33 +0200

    retrofited bugfixes from rwanda

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,14 +74,14 @@ class UsersController < ApplicationController
   # set the user's 'current response' based on the given Request id
   def set_request
     change_user_current_response(DataRequest.find(params[:id]))
-    redirect_to :back
+    redirect_back
   end
 
   # set the user's 'current response' to the one associated with the latest Request
   def set_latest_request
     current_user.set_current_response_to_latest!
     flash[:notice] = latest_request_message(current_user.current_response.request)
-    redirect_to :back
+    redirect_back
   end
 
   protected
@@ -122,6 +122,11 @@ class UsersController < ApplicationController
     end
 
   private
+    def redirect_back
+      redirect_to :back
+      rescue ActionController::RedirectBackError
+        redirect_to dashboard_path
+    end
 
     def load_users
       scope = current_user.organization.users

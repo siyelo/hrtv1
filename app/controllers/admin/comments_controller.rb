1d4e423c7b7f406627b60c6bd366e885d5ab2c1c
Peter Garbers <peter.garbers@gmail.com>
2011-07-28 17:28:22 +0200

    untracked files

---
+++ app/controllers/admin/comments_controller.rb
@@ -0,0 +1,20 @@
+class Admin::CommentsController < Admin::BaseController
+  
+  inherit_resources
+  
+  def index
+    @comments = Comment.paginate(:per_page => 20, :page => params[:page], :order => 'created_at DESC')
+  end
+  
+  def update
+    update! do |format|
+      format.html { redirect_to admin_comments_url }
+    end
+  end
+  
+  def destroy
+    destroy! do |format|
+      format.html { redirect_to admin_comments_url }
+    end
+  end
+end

a2dfc3225fccf22409bd1be4b21923d0ec7fd57a
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-07-28 14:15:21 +0200

    Refactored purposes JS object to accept single/multi options

--- public/javascripts/classification.js
+++ public/javascripts/classification.js
@@ -1,8 +1,8 @@
 var purposes = {
+  type: 'multi',
   lastIndex: 0,
-  setLastIndex: function (index) {
-    purposes.lastIndex = index;
-  },
+  lastId: '',
+
   resetMcdropdown: function (mcdropdown) {
     // reset and focus search
     mcdropdown.find('input:hidden').val('');
@@ -11,6 +11,8 @@ var purposes = {
     //mcdropdown.find('a').trigger('click');
 
     $('.mcdropdown_autocomplete').hide();
+
+    // triger ESC event on mcdropdown
     e = $.Event('keypress');
     e.which = 27; // ESC
     mcdropdown.find('input.purpose_search').trigger(e);
@@ -23,7 +25,7 @@ var purposes = {
     elements.mcDropdown(clone, {
       hoverOutDelay: 0,
       hoverOverDelay: 300,
-//      showACOnEmptyFocus: true,
+      // showACOnEmptyFocus: true,
       allowParentSelect: true,
       delim: ">",
       select: purposes.select_purpose,
@@ -33,7 +35,7 @@ var purposes = {
     })
   },
 
-  add_purpose: function (add_link, lastId) {
+  add_purpose: function (add_link) {
     // prevent adding new purpose if last is active & blank
     if (add_link.hasClass('disabled')) {
       return;
@@ -51,19 +53,7 @@ var purposes = {
     // disable this add purpose button
     add_link.addClass('disabled');
 
-    //var tr = $('<tr/>').append(
-      //$('<td/>').attr({'class': 'desc wrap-60'}).append(
-        //$('<input/>').attr({'class': 'purpose_search', 'type': 'text'})
-      //),
-      //$('<td/>').attr({'class': 'total'}).append(
-        //$('<input/>').attr({'class': 'js_ca', 'type': 'text'})
-      //),
-      //$('<td/>').attr({'class': 'actions'}).append(
-        //$('<img/>').attr({src: "/images/delete_row.png", class: "js_remove_purpose delete_row pointer"})
-      //)
-    //)
     var tr = $(_purpose_row);
-
 
     add_link.parents('tr:first').before(tr);
 
@@ -73,12 +63,14 @@ var purposes = {
     purposes.initMcDropdown(purpose_search);
     purposes.resetMcdropdown($('.mcdropdown'));
 
-    var selected_li = $('#purpose_menu li[rel=' + lastId + ']');
+    var selected_li = $('#purpose_menu li[rel="' + purposes.lastId + '"]');
     if (selected_li.length > 0) {
       McDropDownGlobalUpdateValue(selected_li);
     }
 
-    purposes.setLastIndex(tr.parents('.js_purpose_row').index());
+    if (purposes.type === 'single') {
+      purposes.lastIndex = tr.parents('.js_purpose_row').index();
+    }
   },
 
   get_purpose_context: function(selected_text) {
@@ -132,7 +124,7 @@ var purposes = {
     var purpose_context = purposes.get_purpose_context(selected_text);
     var tr              = mcdropdown.parents('tr:first');
     var td              = mcdropdown.parents('td:first');
-    lastId              = selected_id;
+    purposes.lastId     = selected_id;
 
     mcdropdown.remove();
     td.html(
@@ -147,15 +139,10 @@ var purposes = {
 
   remove_purpose: function (element) {
     var tr          = element.parents('tr:first');
-    var id          = tr.attr('data-ca_id');
-    var loader      = element.next('.ajax-loader');
-    var purpose_row = element.parents('.js_purpose_row:first');
     tr.find('.js_ca').val(0).trigger('keyup');
     tr.remove();
 
-    if (purpose_row.find('.mcdropdown')) {
-      purpose_row.find('.js_add_purpose').removeClass('disabled');
-    }
+    $('.js_add_purpose').removeClass('disabled');
 
     //$('.mcdropdown_autocomplete').remove()
     $('.mcdropdown_menu').hide();
@@ -191,7 +178,7 @@ var purposes = {
       purposes.remove_purpose(mcdropdown)
 
       alert('"' + name + '" is already added');
-      lastId              = purposes.get_selected_val(mcdropdown);
+      purposes.lastId = purposes.get_selected_val(mcdropdown);
       return false;
     } else {
       if (value) {
@@ -199,10 +186,79 @@ var purposes = {
         $('.js_add_purpose').removeClass('disabled');
       }
 
-      tr.attr("data-ca_id", value)
-      tr.find('.total input').attr('id', 'classifications_' + activity_id + '_' + value)
-      tr.find('.total input').attr('name', 'classifications[' + activity_id + '][' + value + ']')
+      if (purposes.type === 'multi') {
+        purposes.update_inputs_multi(tr, value);
+      } else {
+        purposes.update_inputs_single(tr, activity_id, value);
+      }
     }
+  },
+  update_inputs_single: function (block, activity_id, value) {
+    var element = block.find('.total input');
+    element.attr('id', 'classifications_' + activity_id + '_' + value);
+    element.attr('name', 'classifications[' + activity_id + '][' + value + ']');
+  },
+  update_inputs_multi: function (block, value) {
+    $.each(block.find(".js_year"), function (index, element) {
+      var element = $(element);
+      element.attr('id', 'classifications_' + value + '_' + index);
+      element.attr('name', 'classifications[' + value + '][' + index + ']');
+    });
+  },
+  openMcDropDown: function (add_purpose_btn) {
+    if (add_purpose_btn.length > 0) {
+      add_purpose_btn.trigger('click');
+    }
+  },
+  removeMcDropDown: function () {
+    var element = $('.mcdropdown').parents('tr:first').find('.js_remove_purpose')
+    purposes.remove_purpose(element);
+  },
+  changeMcDropDown: function (purpose_row) {
+    if (purpose_row.length > 0) {
+      purposes.removeMcDropDown();
+      purposes.openMcDropDown(purpose_row.find('.js_add_purpose:first'));
+    }
+  },
+
+
+  init: function (type) {
+    purposes.type = type;
+    $(".js_add_purpose").live('click', function (e) {
+      e.preventDefault();
+      purposes.add_purpose($(this));
+    });
+
+    $(".js_remove_purpose").live('click', function (e) {
+      e.preventDefault();
+      if ( confirm('Are you sure?') ) {
+        purposes.remove_purpose($(this));
+      }
+    });
+
+    $(document).bind('keypress', function (e) {
+      //console.log(e.keyCode);
+
+      if (e.keyCode == 13) { // enter key
+        e.preventDefault();
+        var add_purpose_button = $('.js_add_purpose:eq(' + purposes.lastIndex + ')')
+
+        if (!add_purpose_button.hasClass('disabled')) {
+          purposes.openMcDropDown(add_purpose_button);
+        }
+      } else if (e.keyCode === 27) { // esc
+        e.preventDefault();
+        purposes.removeMcDropDown();
+      } else if (e.shiftKey && e.keyCode === 40) { // key down
+        e.preventDefault();
+        var purpose_row = $('.mcdropdown').parents('.js_purpose_row').next('.js_purpose_row')
+        purposes.changeMcDropDown(purpose_row);
+      } else if (e.shiftKey && e.keyCode === 38) { // key up
+        e.preventDefault();
+        var purpose_row = $('.mcdropdown').parents('.js_purpose_row').prev('.js_purpose_row')
+        purposes.changeMcDropDown(purpose_row);
+      }
+    })
   }
 };
 
@@ -212,7 +268,6 @@ var purposes = {
 //###################################
 var classifications_edit = {
   run: function () {
-    var lastId    = '';
     var getClassificationTotal = function (amounts, amount) {
       var total = 0;
       for (var i = 0; i < amounts.length; i++) {
@@ -230,19 +285,7 @@ var classifications_edit = {
       return total;
     };
 
-
-    $(".js_add_purpose").live('click', function (e) {
-      e.preventDefault();
-      purposes.add_purpose($(this), lastId);
-    });
-
-    $(".js_remove_purpose").live('click', function (e) {
-      e.preventDefault();
-      if ( confirm('Are you sure?') ) {
-        purposes.remove_purpose($(this));
-      }
-    });
-
+    // amount totals
     $('.js_ca').live('keyup', function (e) {
       var element = $(this);
       var tr = element.parents('tr.js_purpose_row');
@@ -260,60 +303,17 @@ var classifications_edit = {
       remaining_box.find('span.js_remaining').text(remaining)
     });
 
+    // tooltip
     $("td.tooltip").live('hover', function() {
       this.setAttribute("title", this.textContent)
     }).tipsy({gravity: 'w', live: true, html: true})
 
-    var openMcDropDown = function (add_purpose_btn) {
-      if (add_purpose_btn.length > 0) {
-        add_purpose_btn.trigger('click');
-      }
-    };
-
-    var removeMcDropDown = function () {
-      var element = $('.mcdropdown').parents('tr:first').find('.js_remove_purpose')
-      purposes.remove_purpose(element);
-    };
-
-
-    var changeMcDropDown = function (purpose_row) {
-      if (purpose_row.length > 0) {
-        removeMcDropDown();
-        openMcDropDown(purpose_row.find('.js_add_purpose:first'));
-      }
-    };
-
-    $(document).bind('keypress', function (e) {
-      //console.log(e.keyCode);
-
-      if (e.keyCode == 13) { // enter key
-        e.preventDefault();
-        var add_purpose_button = $('.js_add_purpose:eq(' + purposes.lastIndex + ')')
-
-        if (!add_purpose_button.hasClass('disabled')) {
-          openMcDropDown(add_purpose_button);
-        }
-      } else if (e.keyCode === 27) { // esc
-        e.preventDefault();
-        removeMcDropDown();
-      } else if (e.shiftKey && e.keyCode === 40) { // key down
-        e.preventDefault();
-        var purpose_row = $('.mcdropdown').parents('.js_purpose_row').next('.js_purpose_row')
-        changeMcDropDown(purpose_row);
-      } else if (e.shiftKey && e.keyCode === 38) { // key up
-        e.preventDefault();
-        var purpose_row = $('.mcdropdown').parents('.js_purpose_row').prev('.js_purpose_row')
-        changeMcDropDown(purpose_row);
-      }
-    })
+    purposes.init('single');
   }
 };
 
 var long_term_budgets_show =  {
   run: function () {
-    $(".js_add_purpose").live('click', function (e) {
-      e.preventDefault();
-      purposes.add_purpose($(this), '');
-    });
+    purposes.init('multi');
   }
 };

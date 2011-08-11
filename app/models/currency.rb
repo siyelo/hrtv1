44fa547f226e6223cb6531b351c51db6b80f99b9
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-08-11 18:03:57 +0200

    Create directly hash, without creating array and then hash

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -2,28 +2,31 @@ class Currency < ActiveRecord::Base
 
   ### Attributes
   attr_accessor :to, :from
-  
+
   ### Validations
   validates_uniqueness_of :conversion
   validates_numericality_of :rate
-  
+
   ### Callbacks
   after_save :reload_currencies
-  
+
   ### Class Methods
   def self.currencies_to_yaml
-    currencies = Currency.all(:select => 'conversion, rate').map { |cur| [cur.conversion, cur.rate]}.flatten
-    Hash[*currencies].to_yaml
+    currencies = {}
+    Currency.all(:select => 'conversion, rate').each do |currency|
+      currencies[currency.conversion] = currency.rate
+    end
+    currencies.to_yaml
   end
-  
+
   private
-  
+
     def reload_currencies
       @cur = Currency.all
       currency_config     = Currency.currencies_to_yaml
       Money.default_bank.import_rates(:yaml, currency_config)
     end
-  
+
 end
 
 

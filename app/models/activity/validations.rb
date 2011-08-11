1811611bb306a98241f051b383eadf6380b625ff
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-08-11 11:00:24 +0200

    Added specs for classification_errors_by_type

--- app/models/activity/validations.rb
+++ app/models/activity/validations.rb
@@ -1,25 +1,26 @@
 module Activity::Validations
 
+  class InvalidClassificationType < StandardError; end
+
   def has_budget_or_spend?
     spend.present? || budget.present?
   end
 
-  # TODO: spec
   def classification_errors_by_type(type)
     errors = []
 
     case type
     when 'purposes'
       errors << 'Purposes by Current Budget are not classified' unless coding_budget_classified?
-      errors << 'Purpsoes by Past Expenditure are not classified' unless coding_spend_classified?
+      errors << 'Purposes by Past Expenditure are not classified' unless coding_spend_classified?
     when 'inputs'
       errors << 'Inputs by Current Budget are not classified' unless coding_budget_cc_classified?
       errors << 'Inputs by Past Expenditure are not classified' unless coding_spend_cc_classified?
     when 'locations'
-      errors << 'Inputs by Current Budget are not classified' unless coding_budget_district_classified?
-      errors << 'Inputs by Past Expenditure are not classified' unless coding_spend_district_classified?
+      errors << 'Locations by Current Budget are not classified' unless coding_budget_district_classified?
+      errors << 'Locations by Past Expenditure are not classified' unless coding_spend_district_classified?
     else
-      raise "Invalid type #{type}".to_yaml
+      raise InvalidClassificationType
     end
 
     errors

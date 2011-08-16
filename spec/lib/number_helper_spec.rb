74ff27761d456ddd1cd7d0cd27f29ff433aa3d2a
Glenn Roberts <glenn.roberts@siyelo.com>
2011-08-16 08:57:49 +0200

    [fixes #16720189] make checkbox tags clickable

--- spec/lib/number_helper_spec.rb
+++ spec/lib/number_helper_spec.rb
@@ -9,24 +9,30 @@ describe NumberHelper do
     Money.default_bank.set_rate(:AOA, :USD, nil)
     Money.default_bank.set_rate(:AOA, :EUR, nil)
   end
-  
+
   it "should not change the amounts when the projects currency and the data_response currency are the same" do
     universal_currency_converter(1000, "EUR","EUR").should == 1000
   end
-  
+
   it "should correctly convert currencies to USD then from USD to the currency when a direct conversion is not possible" do
     universal_currency_converter(1000, "GBP", "EUR").should == 1440.92219
   end
-  
+
   it "should correctly convert currencies correctly when a direct conversion is possible" do
    universal_currency_converter(1000, "RWF", "KES").should == 5000
   end
-  
+
   it "when a conversion to USD is not possible by one currency and one of the currencies is not USD" do
     universal_currency_converter(1000, "AOA", "USD").should == 1000
   end
-  
+
   it "when a conversion to USD is not possible by one currency" do
     universal_currency_converter(1000, "AOA", "EUR").should == 1000
+  end
+
+  describe "#n2cs" do
+    it "should format number with currency in front" do
+      n2cs("12345.67", "USD").should == "<span class=\"currency\">USD</span> 12,345.67"
+    end
   end
 end

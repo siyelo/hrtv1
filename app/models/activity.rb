dfba0fddea3241e700b010792cf2d1894ae7895d
Glenn Roberts <glenn.roberts@siyelo.com>
2011-08-22 11:43:58 +0200

    fix weirdness where sub-activities were being created with sub-activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -139,8 +139,10 @@ class Activity < ActiveRecord::Base
 
   def initialize(*params)
     super(*params)
-    #needed for sub activity initialization
-    self.sub_activities.build(:provider_id => self.organization.id) if self.data_response_id && self.sub_activities.empty?
+    unless is_sub_activity?
+      #needed for sub activity initialization
+      self.sub_activities.build(:provider_id => self.organization.id) if self.data_response_id && self.sub_activities.empty?
+    end
   end
 
   ### Class Methods

996ea014164d472c7cd9c286fe6ec3076e296035
Peter Garbers <peter.garbers@gmail.com>
2011-08-26 10:01:04 +0200

    New Coding tree values inserted

---
+++ db/fixes/20110825_add_new_codes.rb
@@ -0,0 +1,50 @@
+require 'fastercsv'
+# Seeding new codes
+
+puts "\n  Loading newCodes.csv..."
+previous_level = 0 #coding tree level
+previous_id = 0 #id of previous coding
+previous_level_1 = 0 #id of the last code with level 1
+previous_level_2 = 0 #id of the last code with level 2
+previous_level_3 = 0 #id of the last code with level 3
+previous_level_4 = 0 #id of the last code with level 4
+previous_level_5 = 0 #id of the last code with level 5
+previous_level_6 = 0 #id of the last code with level 6
+previous_level_7 = 0 #id of the last code with level 7
+previous_level_8 = 0 #id of the last code with level 8
+
+FasterCSV.foreach("db/seed_files/newCodes.csv", :headers=>true) do |row|
+  r = row.to_a
+  r.delete_if { |a| (a[0].nil? || a[1].nil?) && a[0] != "ID" }
+  unless r[0].nil?
+    level = r[0][0].to_i
+
+    parent_id = previous_id if level > previous_level
+    parent_id = Code.find(previous_id).parent_id if level == previous_level
+    parent_id = eval("previous_level_#{level -1}") if level < previous_level && level != 1
+    parent_id = nil if level == 1
+
+    simple_display = r[0][1].gsub(/@/, ',')
+    description = row["Description"].nil? ? '' : row["Description"].gsub(/@/, ',')
+    hssp2_stratobj_val = row["HSSP2 Strategic Objective"].nil? ? '' : row["HSSP2 Strategic Objective"].gsub(/@/, ',')
+    hssp2_stratprog_val = row["HSSP2 Strategic Program"].nil? ? '' : row["HSSP2 Strategic Program"].gsub(/@/, ',')
+    official_name = row["Official (long) name"].nil? ? '' : row["Official (long) name"].gsub(/@/, ',')
+
+    new_code = Code.find(:first, :conditions => {:parent_id => parent_id, :short_display => simple_display})
+
+    unless new_code
+      puts "Adding #{r[0][1]}"
+      new_code = Code.create!(
+        :short_display => simple_display,
+        :description => description,
+        :type => row["Type"],
+        :hssp2_stratobj_val => hssp2_stratobj_val,
+        :hssp2_stratprog_val => hssp2_stratprog_val,
+        :official_name => official_name,
+        :parent_id => parent_id)
+    end
+    previous_level = level.to_i
+    previous_id = row["ID"].present? ? row["ID"] : new_code.id
+    eval("previous_level_#{level} = #{previous_id}")
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110818155523) do
+ActiveRecord::Schema.define(:version => 20110822123733) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
@@ -180,6 +180,15 @@ ActiveRecord::Schema.define(:version => 20110818155523) do
     t.datetime "updated_at"
   end
 
+  create_table "field_helps", :force => true do |t|
+    t.string   "attribute_name"
+    t.string   "short"
+    t.text     "long"
+    t.integer  "model_help_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "funding_flows", :force => true do |t|
     t.integer  "organization_id_from"
     t.integer  "organization_id_to"
@@ -225,6 +234,15 @@ ActiveRecord::Schema.define(:version => 20110818155523) do
     t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "model_helps", :force => true do |t|
+    t.string   "model_name"
+    t.string   "short"
+    t.text     "long"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "comments_count", :default => 0
   end
 
   create_table "organizations", :force => true do |t|

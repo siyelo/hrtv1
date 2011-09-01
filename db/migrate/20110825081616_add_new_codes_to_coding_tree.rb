d3bfbc2e4f4a1647f7653b74b3d8c1e4800215b3
Peter Garbers <peter.garbers@gmail.com>
2011-09-01 16:17:43 +0200

    Fixes migration adding new codes

--- db/migrate/20110825081616_add_new_codes_to_coding_tree.rb
+++ db/migrate/20110825081616_add_new_codes_to_coding_tree.rb
@@ -1,5 +1,6 @@
 class AddNewCodesToCodingTree < ActiveRecord::Migration
   def self.up
+    Code.reset_column_information
     if Rails.env != "test"
       load 'db/fixes/20110825_add_new_codes.rb'
     end

451435670fa43b0c33be432dc5f6f8e0b009131c
Peter Garbers <peter.garbers@gmail.com>
2011-09-01 15:00:50 +0200

    Removing code assignments with multiple percentage decimal places (rounds to 2)

--- db/schema.rb
+++ db/schema.rb
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(:version => 20110830093315) do
     t.decimal  "cached_amount_in_usd", :default => 0.0
   end
 
+  add_index "code_assignments", ["activity_id", "code_id", "type"], :name => "index_code_assignments_on_activity_id_and_code_id_and_type"
   add_index "code_assignments", ["code_id"], :name => "index_code_assignments_on_code_id"
 
   create_table "codes", :force => true do |t|
@@ -121,6 +122,16 @@ ActiveRecord::Schema.define(:version => 20110830093315) do
   add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
   add_index "comments", ["commentable_type"], :name => "index_comments_on_commentable_type"
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "commodities", :force => true do |t|
+    t.string   "commodity_type"
+    t.text     "description"
+    t.decimal  "unit_cost",        :default => 0.0
+    t.integer  "quantity"
+    t.integer  "data_response_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "currencies", :force => true do |t|
     t.string   "conversion"
@@ -200,6 +211,15 @@ ActiveRecord::Schema.define(:version => 20110830093315) do
   add_index "funding_flows", ["project_id"], :name => "index_funding_flows_on_project_id"
   add_index "funding_flows", ["self_provider_flag"], :name => "index_funding_flows_on_self_provider_flag"
 
+  create_table "funding_sources", :force => true do |t|
+    t.integer  "activity_id"
+    t.integer  "funding_flow_id"
+    t.decimal  "spend"
+    t.decimal  "budget"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "funding_streams", :force => true do |t|
     t.integer  "project_id"
     t.integer  "organization_ufs_id"
@@ -217,6 +237,11 @@ ActiveRecord::Schema.define(:version => 20110830093315) do
     t.text     "message"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "locations_organizations", :id => false, :force => true do |t|
+    t.integer "location_id"
+    t.integer "organization_id"
   end
 
   create_table "organizations", :force => true do |t|
@@ -313,8 +338,5 @@ ActiveRecord::Schema.define(:version => 20110830093315) do
     t.boolean  "active",                   :default => false
     t.integer  "location_id"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email"
-  add_index "users", ["perishable_token"], :name => "index_users_on_perishable_token"
 
 end

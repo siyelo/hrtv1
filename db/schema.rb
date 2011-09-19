f3073a971ef815c1a39f7cb0bf162b1581e47008
Peter Garbers <peter.garbers@gmail.com>
2011-09-19 17:49:34 +0200

    Fixed your_projects_class on review screen

--- db/schema.rb
+++ db/schema.rb
@@ -87,7 +87,6 @@ ActiveRecord::Schema.define(:version => 20110916153117) do
     t.decimal  "cached_amount_in_usd", :default => 0.0
   end
 
-  add_index "code_assignments", ["activity_id", "code_id", "type"], :name => "index_code_assignments_on_activity_id_and_code_id_and_type"
   add_index "code_assignments", ["code_id"], :name => "index_code_assignments_on_code_id"
 
   create_table "codes", :force => true do |t|
@@ -327,5 +326,8 @@ ActiveRecord::Schema.define(:version => 20110916153117) do
     t.datetime "current_login_at"
     t.datetime "last_login_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["perishable_token"], :name => "index_users_on_perishable_token"
 
 end

3bbe2c3341eb772f69f304c5c02939e234e7126a
Sachin Ranchod <sachin.ranchod@gmail.com>
2011-09-20 14:45:09 +0200

    [fixes #18524391] Fix Airbrake due to iconv translit not working on Heroku

--- lib/encoding_helper.rb
+++ lib/encoding_helper.rb
@@ -1,11 +1,15 @@
 module EncodingHelper
   def self.sanitize_encoding(string)
-    begin
-      Iconv.conv("UTF-8//TRANSLIT//IGNORE", "UTF8", string)
-    rescue
-      # //IGNORE ignores invalid byte sequences unless they occur
-      # right at the end of the string
-      Iconv.conv("UTF-8//TRANSLIT//IGNORE", "UTF8", string + ' ')[0..-1]
+    if string
+      begin
+        # //IGNORE ignores invalid byte sequences unless they occur
+        # right at the end of the string
+        # http://po-ru.com/diary/fixing-invalid-utf-8-in-ruby-revisited/
+        Iconv.conv("UTF-8//TRANSLIT//IGNORE", "UTF8", string + ' ')[0..-2]
+      rescue Iconv::IllegalSequence
+        # //TRANSLIT does not work consistently in all environments
+        Iconv.conv("UTF-8//IGNORE", "UTF8", string + ' ')[0..-2]
+      end
     end
   end
 end

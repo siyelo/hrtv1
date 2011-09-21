0324bf14d14c25cadc60c1b643af57af50632260
Glenn Roberts <glenn.roberts@siyelo.com>
2011-09-21 16:29:22 +0200

    [fixes #18592519] fix bug: admin can edit non-reporting org settings, add specs

--- spec/controllers/organizations_controller_spec.rb
+++ spec/controllers/organizations_controller_spec.rb
@@ -1,21 +1,45 @@
 require 'spec_helper'
 
+shared_examples_for 'an organization controller' do
+  it "should allow admin to edit settings of reporting organization" do
+    o = Factory :organization
+    get :edit, :id => o.id
+    response.should be_success
+  end
+  it "should allow admin to edit settings of nonreporting org" do
+    o = Factory :organization, :raw_type => 'Communal FOSA'
+    get :edit, :id => o.id
+    response.should be_success
+  end
+end
+
 describe OrganizationsController do
-  before :each do
-    @reporter = Factory.create(:reporter)
-    login(@reporter)
+  context "as a reporter" do
+    before :each do
+      login(Factory.create(:reporter))
+    end
+
+    it "redirects to dashboard_path" do
+      put :update, :id => :current
+      response.should redirect_to(dashboard_path)
+    end
+
+    it "downloads csv template" do
+      Organization.should_receive(:download_template).and_return('csv')
+      get :export
+      response.should be_success
+      response.header["Content-Type"].should == "text/csv; charset=iso-8859-1; header=present"
+      response.header["Content-Disposition"].should == "attachment; filename=organizations.csv"
+    end
+
+    it_should_behave_like 'an organization controller'
   end
 
-  it "redirects to dashboard_path" do
-    put :update, :id => :current
-    response.should redirect_to(dashboard_path)
-  end
-  
-  it "downloads csv template" do
-    Organization.should_receive(:download_template).and_return('csv')
-    get :export
-    response.should be_success
-    response.header["Content-Type"].should == "text/csv; charset=iso-8859-1; header=present"
-    response.header["Content-Disposition"].should == "attachment; filename=organizations.csv"
+  context "as a sysadmin" do
+    before :each do
+      login(Factory.create(:admin))
+    end
+
+    it_should_behave_like 'an organization controller'
   end
 end

091ab3d568471b8702fe7433ecc6d2b0cc102295
Peter Garbers <peter.garbers@gmail.com>
2011-09-27 16:21:11 +0200

    Changing as per code review

--- db/fixes/20110927_merge_concern_worldwide.rb
+++ db/fixes/20110927_merge_concern_worldwide.rb
@@ -1,3 +1,3 @@
-@target = Organization.find(3676)
-@duplicate = Organization.find(3977)
+@target = Organization.find_by_id 3676
+@duplicate = Organization.find_by_id 3977
 Organization.merge_organizations!(@target, @duplicate) if @target && @duplicate

1a6d4f03478f829fb1ff27282fa0ab2b837730a6
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-09-27 16:37:35 +0200

    We need this fix for staging env also

--- db/migrate/20110927120958_deduplicate_concern.rb
+++ db/migrate/20110927120958_deduplicate_concern.rb
@@ -1,6 +1,6 @@
 class DeduplicateConcern < ActiveRecord::Migration
   def self.up
-    load 'db/fixes/20110927_merge_concern_worldwide.rb' if Rails.env == "production"
+    load 'db/fixes/20110927_merge_concern_worldwide.rb'
   end
 
   def self.down

68ad4963e96f25dfa315cba5f27d9478863597ec
Glenn Roberts <glenn.roberts@siyelo.com>
2011-10-10 11:25:31 +0200

    [fixes #19395591] long project names break activity.edit form layout

---
+++ app/helpers/shared/outlays_helper.rb
@@ -0,0 +1,10 @@
+module Shared::OutlaysHelper
+  def sorted_project_select(response, klass, length = 60)
+    list = response.projects.sort_by{ |p| p.name }.collect do |u|
+      [ truncate(u.name, length), u.id ]
+    end
+    list = list.insert(0,["<Automatically create a project for me>", -1])
+    list = list.insert(1,["<No project>", nil]) if klass == "OtherCost"
+    [['Select a project...', '']] + list
+  end
+end

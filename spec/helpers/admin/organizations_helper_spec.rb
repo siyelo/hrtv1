275b2eb31e6a8bc5429af6e4ecab077d4fa771d5
Sachin Ranchod <sachin.ranchod@gmail.com>
2011-10-11 10:03:38 +0200

    [fixes #19515133] Link to correct response in org listing

--- spec/helpers/admin/organizations_helper_spec.rb
+++ spec/helpers/admin/organizations_helper_spec.rb
@@ -5,7 +5,7 @@ describe Admin::OrganizationsHelper do
   describe "#return_organization_response" do
     it "should return the correct response for the organization" do
       basic_setup_response
-      OrganizationsHelper.organization_response(@organization).should == @response
+      OrganizationsHelper.organization_response(@organization, @request).should == @response
     end
 
     it "should return the correct response for the organization" do
@@ -13,7 +13,7 @@ describe Admin::OrganizationsHelper do
       org = Factory(:organization)
       req = Factory(:data_request)
       dr1 = org.latest_response
-      OrganizationsHelper.organization_response(org).should_not == @response
+      OrganizationsHelper.organization_response(org, req).should_not == @response
     end
   end
 end

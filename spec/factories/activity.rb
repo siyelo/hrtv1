67be6e39ff816693f818c7c08a9c404fabf4e805
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-10-19 14:26:25 +0200

    Fixed failing specs

--- spec/factories/activity.rb
+++ spec/factories/activity.rb
@@ -1,8 +1,6 @@
 Factory.define :activity, :class => Activity do |f|
   f.sequence(:name) { |i| "activity_name_#{i}_#{rand(100_000_000)}" }
   f.description     { 'activity_description' }
-  f.start_date      { "2010-01-01" }
-  f.end_date        { "2010-12-31" }
 end
 
 Factory.define :classified_activity, :class => Activity, :parent => :activity do |f|

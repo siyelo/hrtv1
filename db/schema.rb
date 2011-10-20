e37826e966e1d9242288930417580bdced4a456d
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2011-10-20 17:13:24 +0200

    [fixes #19945709] Remove purpose and input classifications for OC

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111019121248) do
+ActiveRecord::Schema.define(:version => 20111020144030) do
 
   create_table "activities", :force => true do |t|
     t.string   "name"
